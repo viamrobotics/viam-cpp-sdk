039ed1b43b7227d9940704609064f38461361aef
Ethan Rodkin <ethanrodkin@protonmail.com>
2023-01-11 13:55:42 -0500

    make proto_type use pointer to proto_type internally

--- src/common/proto_type.cc
+++ src/common/proto_type.cc
@@ -10,25 +10,25 @@ using google::protobuf::Struct;
 using google::protobuf::Value;
 
 class ProtoType;
-Struct map_to_struct(std::unordered_map<std::string, ProtoType> dict);
-std::unordered_map<std::string, ProtoType> struct_to_map(Struct struct_);
+Struct map_to_struct(std::unordered_map<std::string, ProtoType*> dict);
+std::unordered_map<std::string, ProtoType*> struct_to_map(Struct struct_);
 
 // float, string, struct, array, struct(map from string to any of the above)
 class ProtoType {
        public:
 	boost::variant<boost::blank, bool, std::string, int, double,
-		       std::unordered_map<std::string, ProtoType>,
-		       std::vector<ProtoType>>
+		       std::unordered_map<std::string, ProtoType*>,
+		       std::vector<ProtoType*>>
 	    proto_type;
 	ProtoType() { proto_type = boost::blank(); }
 	ProtoType(bool b) { proto_type = b; }
 	ProtoType(std::string s) { proto_type = s; }
 	ProtoType(int i) { proto_type = i; }
 	ProtoType(double d) { proto_type = d; }
-	ProtoType(std::unordered_map<std::string, ProtoType> m) {
+	ProtoType(std::unordered_map<std::string, ProtoType*> m) {
 		proto_type = m;
 	}
-	ProtoType(std::vector<ProtoType> v) { proto_type = v; }
+	ProtoType(std::vector<ProtoType*> v) { proto_type = v; }
 	ProtoType(Value value);
 	Value proto_value();
 };
@@ -48,18 +48,18 @@ ProtoType::ProtoType(Value value) {
 			break;
 		}
 		case Value::KindCase::kListValue: {
-			std::vector<ProtoType> vec;
+			std::vector<ProtoType*> vec;
 			google::protobuf::ListValue list_val =
 			    value.list_value();
 			for (auto val : value.list_value().values()) {
 				ProtoType p = ProtoType(val);
-				vec.push_back(p);
+				vec.push_back(&p);
 			}
-			proto_type = vec;
+			proto_type = &vec;
 			break;
 		}
 		case Value::KindCase::kStructValue: {
-			std::unordered_map<std::string, ProtoType> map =
+			std::unordered_map<std::string, ProtoType*> map =
 			    struct_to_map(value.struct_value());
 			proto_type = map;
 			break;
@@ -72,11 +72,11 @@ ProtoType::ProtoType(Value value) {
 	}
 };
 
-Struct map_to_struct(std::unordered_map<std::string, ProtoType> dict) {
+Struct map_to_struct(std::unordered_map<std::string, ProtoType*> dict) {
 	Struct s;
 	for (auto& key_and_value : dict) {
 		const std::string key = key_and_value.first;
-		Value value = key_and_value.second.proto_value();
+		Value value = key_and_value.second->proto_value();
 		google::protobuf::MapPair<std::string, Value> val(key, value);
 		s.mutable_fields()->insert(val);
 	}
@@ -84,13 +84,13 @@ Struct map_to_struct(std::unordered_map<std::string, ProtoType> dict) {
 	return s;
 }
 
-std::unordered_map<std::string, ProtoType> struct_to_map(Struct struct_) {
+std::unordered_map<std::string, ProtoType*> struct_to_map(Struct struct_) {
 	google::protobuf::Map<std::string, Value> struct_map = struct_.fields();
-	std::unordered_map<std::string, ProtoType> map;
+	std::unordered_map<std::string, ProtoType*> map;
 	for (auto val : struct_.fields()) {
 		std::string key = val.first;
 		ProtoType value(val.second);
-		std::pair<std::string, ProtoType> pair(key, value);
+		std::pair<std::string, ProtoType*> pair(key, &value);
 		map.insert(pair);
 	}
 
@@ -127,9 +127,9 @@ Value ProtoType::proto_value() {
 			break;
 		}
 		case 5: {
-			std::unordered_map<std::string, ProtoType> map =
+			std::unordered_map<std::string, ProtoType*> map =
 			    boost::get<
-				std::unordered_map<std::string, ProtoType>>(
+				std::unordered_map<std::string, ProtoType*>>(
 				proto_type);
 			Struct s = map_to_struct(map);
 			*v.mutable_struct_value() = s;
@@ -140,10 +140,10 @@ Value ProtoType::proto_value() {
 			    *v.mutable_list_value();
 			google::protobuf::RepeatedPtrField<Value>* values =
 			    l.mutable_values();
-			std::vector<ProtoType> vec =
-			    boost::get<std::vector<ProtoType>>(proto_type);
+			std::vector<ProtoType*> vec =
+			    boost::get<std::vector<ProtoType*>>(proto_type);
 			for (auto val : vec) {
-				*values->Add() = val.proto_value();
+				*values->Add() = val->proto_value();
 			}
 			break;
 		}
