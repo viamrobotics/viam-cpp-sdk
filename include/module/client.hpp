e96b48d727556fa910a87933ae89a07d44d1a867
Ethan Rodkin <ethanrodkin@protonmail.com>
2023-01-28 09:15:16 -0500

    add module client header

---
+++ include/module/client.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <module/v1/module.grpc.pb.h>
+
+#include <config/resource.hpp>
+#include <module/handler_map.hpp>
+#include <mutex>
+#include <rpc/dial.hpp>
+
+class ModuleClient {
+   public:
+    std::mutex lock;
+    std::unique_ptr<viam::module::v1::ModuleService::Stub> stub_;
+    HandlerMap_ handles;
+
+    ModuleClient(Viam::SDK::ViamChannel channel);
+    void add_resource(Component cfg, std::vector<std::string> dependencies);
+
+    void reconfigure_resource(Component cfg, std::vector<std::string> dependencies);
+    void remove_resource(Name name);
+
+    void ready(std::string address);
+};
+
