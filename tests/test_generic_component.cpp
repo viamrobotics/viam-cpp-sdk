e4cf8f9ceaca8ddb3a0b6b975eb7d22a031b8548
Ethan Rodkin <ethan@viam.com>
2023-03-10 09:57:41 -0500

    get ctest working

--- tests/test_generic_component.cpp
+++ tests/test_generic_component.cpp
@@ -5,7 +5,12 @@ BOOST_AUTO_TEST_SUITE(generic_suite)
 
 BOOST_AUTO_TEST_CASE(test1) {
     int i = 1;
-    BOOST_CHECK(i * i == i);
+    BOOST_CHECK(i * i == 1);
+}
+
+BOOST_AUTO_TEST_CASE(test2) {
+    int i = 1;
+    BOOST_CHECK(i + i == 2);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
