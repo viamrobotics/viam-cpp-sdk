49e74a23a58b435aa0a5f742a29ebf363960b398
Ethan Rodkin <ethan@viam.com>
2023-03-10 08:40:54 -0500

    tests building in cmake

--- tests/test_generic_component.cpp
+++ tests/test_generic_component.cpp
@@ -1,27 +1,11 @@
-#include <memory>
-#define BOOST_TEST_MODULE generic_test
-#include <boost/bind/bind.hpp>
-#include <boost/test/unit_test.hpp>
-
-class generic_test {
-   public:
-    void test_method1() {
-        BOOST_TEST(true);
-    }
-    void test_method2() {
-        BOOST_TEST(false);
-    }
-};
+#define BOOST_TEST_MODULE test module test_generic_component
+#include <boost/test/included/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE(generic_suite)
 
-boost::unit_test::test_suite* unit_test_suite(int argc, char* argv[]) {
-    std::shared_ptr<generic_test> tester = std::make_shared<generic_test>();
-    boost::unit_test::framework::master_test_suite().add(
-        BOOST_TEST_CASE(boost::bind(&generic_test::test_method1, tester)));
-    boost::unit_test::framework::master_test_suite().add(
-        BOOST_TEST_CASE(boost::bind(&generic_test::test_method2, tester)));
-    return 0;
-};
+BOOST_AUTO_TEST_CASE(test1) {
+    int i = 1;
+    BOOST_CHECK(i * i == i);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
