9e425f4d1e3f3f7f0b23e43dc680855fec138b77
Ethan Rodkin <ethan@viam.com>
2023-04-04 13:33:34 -0400

    add runtest

---
+++ etc/docker/run_test.sh
@@ -0,0 +1,9 @@
+echo "hello"
+echo "world"
+
+#mkdir build 
+#cd build 
+#pwd 
+#cmake .. -G Ninja -DVIAMCPPSDK_USE_DYNAMIC_PROTOS=ON -DVIAMCPPSDK_OFFLINE_PROTO_GENERATION=ON 
+#cd src/tests 
+#ctest
