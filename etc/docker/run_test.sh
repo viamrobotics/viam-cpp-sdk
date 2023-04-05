ea935f9ac7e1aa8f3dcd9f60c4adaca3faf3ea5c
Cheuk Tse <cheuk@viam.com>
2023-04-05 12:42:58 -0400

    kinda works

--- etc/docker/run_test.sh
+++ etc/docker/run_test.sh
@@ -1,9 +1,9 @@
-echo "hello"
-echo "world"
-
-#mkdir build 
-#cd build 
-#pwd 
-#cmake .. -G Ninja -DVIAMCPPSDK_USE_DYNAMIC_PROTOS=ON -DVIAMCPPSDK_OFFLINE_PROTO_GENERATION=ON 
-#cd src/tests 
-#ctest
+#!/bin/bash
+mkdir build 
+cd build 
+pwd 
+cmake .. -G Ninja -DVIAMCPPSDK_USE_DYNAMIC_PROTOS=ON -DVIAMCPPSDK_OFFLINE_PROTO_GENERATION=ON 
+ninja all
+ninja install
+cd src/tests 
+ctest
