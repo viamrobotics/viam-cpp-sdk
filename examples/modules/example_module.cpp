cb106a48c36a502b8d8aa711f157817e72ca77b1
Benji Rewis <benji.rewis@viam.com>
2023-04-10 13:03:56 -0400

    elaborate on sigterm comment

--- examples/modules/example_module.cpp
+++ examples/modules/example_module.cpp
@@ -73,7 +73,8 @@ int main(int argc, char** argv) {
 
     // C++ modules must handle SIGINT and SIGTERM. Make sure to create a sigset
     // for SIGINT and SIGTERM that can be later awaited in a thread that cleanly
-    // shuts down your module.
+    // shuts down your module. pthread_sigmask should be called near the start
+    // of main so that later threads inherit the mask.
     sigset_t sigset;
     sigemptyset(&sigset);
     sigaddset(&sigset, SIGINT);
