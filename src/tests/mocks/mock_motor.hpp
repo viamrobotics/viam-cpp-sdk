4843d11be341c3da9de9a9b08c97fa821765ce20
zaporter-work <work-github@zackporter.com>
2023-04-18 16:55:23 -0400

    Cleanup imports for mock

--- src/tests/mocks/mock_motor.hpp
+++ src/tests/mocks/mock_motor.hpp
@@ -12,16 +12,18 @@ namespace viam {
 namespace cppsdktests {
 namespace motor {
 
-class MockMotor : public viam::cppsdk::Motor {
+using viam::cppsdk::Motor;
+
+class MockMotor : public Motor {
    public:
     void set_power(double power_pct) override;
     void go_for(double rpm, double revolutions) override;
     void go_to(double rpm, double position_revolutions) override;
     void reset_zero_position(double offset) override;
-    viam::cppsdk::Motor::position get_position() override;
-    viam::cppsdk::Motor::properties get_properties() override;
+    Motor::position get_position() override;
+    Motor::properties get_properties() override;
     void stop_motor() override;
-    viam::cppsdk::Motor::power_status get_power_status() override;
+    Motor::power_status get_power_status() override;
     bool is_moving() override;
     viam::cppsdk::AttributeMap do_command(viam::cppsdk::AttributeMap command) override;
     static std::shared_ptr<MockMotor> get_mock_motor();
@@ -29,16 +31,16 @@ class MockMotor : public viam::cppsdk::Motor {
     MockMotor(std::string name) : Motor(std::move(name)){};
 
    private:
-    viam::cppsdk::Motor::position position;
-    viam::cppsdk::Motor::power_status power_status;
-    viam::cppsdk::Motor::properties properties;
+    Motor::position position;
+    Motor::power_status power_status;
+    Motor::properties properties;
     // TODO(RSDK-2747) swap to AttributeMap
     std::shared_ptr<std::unordered_map<std::string, std::shared_ptr<viam::cppsdk::ProtoType>>> map;
 };
 
-viam::cppsdk::Motor::position fake_position();
-viam::cppsdk::Motor::power_status fake_power_status();
-viam::cppsdk::Motor::properties fake_properties();
+Motor::position fake_position();
+Motor::power_status fake_power_status();
+Motor::properties fake_properties();
 
 }  // namespace motor
 }  // namespace cppsdktests
--- src/tests/test_motor.cpp
+++ src/tests/test_motor.cpp
@@ -25,28 +25,30 @@ using namespace viam::cppsdk;
 
 BOOST_AUTO_TEST_SUITE(test_mock)
 
-BOOST_AUTO_TEST_CASE(test_set_power) {
+BOOST_AUTO_TEST_CASE(mock_set_power) {
     std::shared_ptr<MockMotor> motor = MockMotor::get_mock_motor();
     motor->set_power(1.0);
     BOOST_CHECK(motor->get_power_status().power_pct == 1.0);
+    BOOST_CHECK(motor->get_power_status().is_on);
     motor->set_power(0.0);
     BOOST_CHECK(motor->get_power_status().power_pct == 0.0);
+    BOOST_CHECK(!motor->get_power_status().is_on);
 }
 
-BOOST_AUTO_TEST_CASE(test_go_for) {
+BOOST_AUTO_TEST_CASE(mock_go_for) {
     std::shared_ptr<MockMotor> motor = MockMotor::get_mock_motor();
     motor->go_for(1.0, 1.0);
     motor->go_for(1.0, 1.5);
     BOOST_CHECK(motor->get_position() == 2.5);
 }
 
-BOOST_AUTO_TEST_CASE(test_go_to) {
+BOOST_AUTO_TEST_CASE(mock_go_to) {
     std::shared_ptr<MockMotor> motor = MockMotor::get_mock_motor();
     motor->go_to(1.0, 1.0);
     BOOST_CHECK(motor->get_position() == 1.0);
 }
 
-BOOST_AUTO_TEST_CASE(test_reset_zero_position) {
+BOOST_AUTO_TEST_CASE(mock_reset_zero_position) {
     std::shared_ptr<MockMotor> motor = MockMotor::get_mock_motor();
     motor->go_to(1.0, 1.0);
     BOOST_CHECK(motor->get_position() == 1.0);
@@ -54,12 +56,12 @@ BOOST_AUTO_TEST_CASE(test_reset_zero_position) {
     BOOST_CHECK(motor->get_position() == -0.5);
 }
 
-BOOST_AUTO_TEST_CASE(test_get_properties) {
+BOOST_AUTO_TEST_CASE(mock_get_properties) {
     std::shared_ptr<MockMotor> motor = MockMotor::get_mock_motor();
     BOOST_CHECK(motor->get_properties().position_reporting);
 }
 
-BOOST_AUTO_TEST_CASE(test_stop_motor) {
+BOOST_AUTO_TEST_CASE(mock_stop_motor) {
     // This test is a no-op for now because is_moving will always
     // return false
     std::shared_ptr<MockMotor> motor = MockMotor::get_mock_motor();
@@ -67,7 +69,7 @@ BOOST_AUTO_TEST_CASE(test_stop_motor) {
     BOOST_CHECK(!motor->is_moving());
 }
 
-BOOST_AUTO_TEST_CASE(test_do_command) {
+BOOST_AUTO_TEST_CASE(mock_do_command) {
     std::shared_ptr<MockMotor> motor = MockMotor::get_mock_motor();
     AttributeMap expected = fake_map();
 
@@ -80,7 +82,7 @@ BOOST_AUTO_TEST_CASE(test_do_command) {
     BOOST_CHECK(result_pt == expected_pt);
 }
 
-BOOST_AUTO_TEST_CASE(test_exception_creation) {
+BOOST_AUTO_TEST_CASE(mock_exception_creation) {
     std::shared_ptr<MockMotor> motor = MockMotor::get_mock_motor();
     BOOST_CHECK_THROW(motor->go_for(0.0, 1.0), std::runtime_error);
 }
