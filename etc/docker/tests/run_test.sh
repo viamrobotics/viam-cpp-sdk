dac7ab4222f7a5efefde31bdb96279a1aa999c33
Ethan Rodkin <ethan@viam.com>
2023-04-24 15:25:31 -0400

    use sanitized build for github CI test

--- etc/docker/tests/run_test.sh
+++ etc/docker/tests/run_test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 mkdir build
 cd build
-cmake .. -G Ninja -DVIAMCPPSDK_USE_DYNAMIC_PROTOS=ON -DVIAMCPPSDK_OFFLINE_PROTO_GENERATION=ON
+cmake .. -G Ninja -DVIAMCPPSDK_USE_DYNAMIC_PROTOS=ON \
+		-DVIAMCPPSDK_OFFLINE_PROTO_GENERATION=ON \
+		-DVIAMCPPSDK_SANITIZED_BUILD=ON
 ninja all
 ninja install
 cd src/viam/sdk/tests
