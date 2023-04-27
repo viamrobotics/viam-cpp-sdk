5a06a92b0c5610a11d2c7b72c3fa12f750a4704d
Zack Porter <121693134+zaporter-work@users.noreply.github.com>
2023-04-27 13:12:29 -0400

    RSDK-2751 Clean up example motor (#88)

--- src/viam/examples/motor/example_motor.cpp
+++ src/viam/examples/motor/example_motor.cpp
@@ -1,67 +1,125 @@
 #include <chrono>
-#include <cstddef>
 #include <fstream>
-#include <functional>
-#include <iostream>
-#include <memory>
-#include <ostream>
 #include <string>
-#include <thread>
-#include <vector>
-
-#include <boost/optional.hpp>
-#include <grpcpp/channel.h>
-#include <grpcpp/client_context.h>
-#include <grpcpp/grpcpp.h>
-#include <grpcpp/support/status.h>
 #include <unistd.h>
-
-#include <viam/api/common/v1/common.pb.h>
-#include <viam/api/robot/v1/robot.grpc.pb.h>
-#include <viam/api/robot/v1/robot.pb.h>
+#include <vector>
 
 #include <viam/sdk/components/motor/client.hpp>
 #include <viam/sdk/components/motor/motor.hpp>
-#include <viam/sdk/components/motor/server.hpp>
 #include <viam/sdk/robot/client.hpp>
 #include <viam/sdk/robot/service.hpp>
 #include <viam/sdk/rpc/dial.hpp>
 
-using namespace viam::sdk;
-using viam::robot::v1::Status;
-
-// TODO(RSDK-2751) Cleanup examples for components
-int main() {
-    DialOptions dial_options;
-    dial_options.allow_insecure_downgrade = true;
-    boost::optional<DialOptions> opts(dial_options);
-
-    std::string address = "localhost:8080";
-    Options options = Options(0, opts);
-    std::shared_ptr<RobotClient> robot = RobotClient::at_address(address, options);
-    robot->refresh();
-    std::cout << "Created robot" << std::endl;
-    std::vector<ResourceName>* resource_names = robot->resource_names();
-
-    std::cout << "List resources of the robot" << std::endl;
-    for (ResourceName resource : *resource_names) {
-        std::cout << "Resource name: " << resource.name() << resource.type() << resource.subtype()
-                  << std::endl;
+void print_motor_position(std::shared_ptr<viam::sdk::MotorClient> motor) {
+    // Whether the motor supports returning its position
+    if (motor->get_properties().position_reporting) {
+        // Position is measured in rotations (position is a typedef double)
+        std::cout << "Motor pos: " << motor->get_position() << std::endl;
+    } else {
+        std::cout << "Motor position unavailable" << std::endl;
     }
-    std::string motor_name = "motor1";
+}
 
-    std::cout << "Getting motor: " << motor_name << std::endl;
-    std::shared_ptr<MotorClient> motor = robot->resource_by_name<MotorClient>(motor_name);
+int main() {
+    using std::cerr;
+    using std::cout;
+    using std::endl;
+    namespace vs = ::viam::sdk;
 
-    Motor::position pos = motor->get_position();
-    std::cout << "Motor position " << pos << std::endl;
+    try {
+        // If you want to connect to a remote robot, this should be the url of the robot
+        // Ex: xxx.xxx.viam.cloud
+        std::string robot_address("localhost:8080");
+        // If you want to connect to a remote robot, you need the robot location secret (not
+        // the part secret) You can find this on app.viam.com
+        vs::Credentials credentials("");
 
-    std::cout << "Moving forward by 1/2 rotation" << std::endl;
-    motor->go_for(1000, 0.5);
+        vs::DialOptions dial_options;
 
-    pos = motor->get_position();
-    std::cout << "Motor position " << pos << std::endl;
+        // If you have credentials, use this to pass them to the robot
+        // dial_options.credentials = credentials;
 
-    robot->close();
-    return 0;
+        // This is for an example. Care should be taken before exercising this option in production.
+        dial_options.allow_insecure_downgrade = credentials.payload.empty();
+
+        // Set the refresh interval of the robot (in seconds) (0 = auto refresh) and the dial
+        // options
+        vs::Options options = vs::Options(1, dial_options);
+
+        std::shared_ptr<vs::RobotClient> robot;
+        try {
+            robot = vs::RobotClient::at_address(robot_address, options);
+            cout << "Successfully connected to the robot" << endl;
+        } catch (const std::exception& e) {
+            cerr << "Failed to connect to the robot. Exiting." << endl;
+            throw;
+        }
+
+        std::vector<vs::ResourceName>* resource_names = robot->resource_names();
+
+        cout << "Resources of the robot:" << endl;
+        for (vs::ResourceName resource : *resource_names) {
+            cout << " - " << resource.name() << " (" << resource.subtype() << ")" << endl;
+        }
+
+        std::string motor_name("motor1");
+
+        cout << "Getting motor: " << motor_name << endl;
+        std::shared_ptr<vs::MotorClient> motor;
+        try {
+            motor = robot->resource_by_name<vs::MotorClient>(motor_name);
+        } catch (const std::exception& e) {
+            cerr << "Failed to find " << motor_name << ". Exiting." << endl;
+            throw;
+        }
+
+        // Motors are always turned off if there is no attached client so this is expected to be 0
+        cout << "Motor power: " << motor->get_power_status().power_pct << endl;
+        cout << "Setting motor power to 50%" << endl;
+        motor->set_power(0.5);
+        cout << "Motor power: " << motor->get_power_status().power_pct << endl;
+
+        print_motor_position(motor);
+
+        cout << "Moving motor by 0.5 rotations at 10rpm" << endl;
+        motor->go_for(10, 0.5);
+        print_motor_position(motor);
+        cout << "Moving motor back to starting location" << endl;
+        if (motor->get_properties().position_reporting) {
+            // More accurate
+            motor->go_to(10, 0);
+        } else {
+            // If position reporting is unavailable, we cannot call go_to
+            // but we can defer to reverting past actions
+            motor->go_for(10, -0.5);
+        }
+        print_motor_position(motor);
+        cout << "Moving motor forward for 2 seconds at 30rpm" << endl;
+        // With `revolutions`==0, the call will be non-blocking
+        // and the motor will rotate indefintely.
+        motor->go_for(30, 0);
+
+        print_motor_position(motor);
+
+        // Wait for 2 seconds, print position every 0.5 seconds
+        for (int i = 0; i < 4; i++) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            print_motor_position(motor);
+        }
+
+        // Explicitly stop the motor
+        cout << "Stopping motor" << endl;
+        vs::AttributeMap stop_map =
+            std::make_shared<std::unordered_map<std::string, std::shared_ptr<vs::ProtoType>>>();
+        motor->stop(stop_map);
+
+    } catch (const std::exception& ex) {
+        cerr << "Program failed. Exception: " << std::string(ex.what()) << endl;
+        return EXIT_FAILURE;
+    } catch (...) {
+        cerr << "Program failed without exception message." << endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
