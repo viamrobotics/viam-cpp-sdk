b7fb8989eb82c267608d5b446c6f71c2a7cd7f79
zaporter-work <work-github@zackporter.com>
2023-05-08 14:59:31 -0400

    Add clang-tidy to docker

--- src/viam/sdk/common/proto_type.cpp
+++ src/viam/sdk/common/proto_type.cpp
@@ -17,10 +17,33 @@ namespace sdk {
 using google::protobuf::Struct;
 using google::protobuf::Value;
 
-// NOLINTBEGIN(readability-redundant-declaration)
-Struct map_to_struct(AttributeMap dict);
-AttributeMap struct_to_map(Struct struct_);
-// NOLINTEND(readability-redundant-declaration)
+// NOLINTNEXTLINE(misc-no-recursion)
+Struct map_to_struct(AttributeMap dict) {
+    Struct s;
+    for (auto& key_and_value : *dict) {
+        const std::string key = key_and_value.first;
+        Value value = key_and_value.second->proto_value();
+        google::protobuf::MapPair<std::string, Value> val(key, value);
+        s.mutable_fields()->insert(val);
+    }
+
+    return s;
+}
+
+// NOLINTNEXTLINE(misc-no-recursion)
+AttributeMap struct_to_map(Struct struct_) {
+    std::shared_ptr<std::unordered_map<std::string, std::shared_ptr<ProtoType>>> map =
+        std::make_shared<std::unordered_map<std::string, std::shared_ptr<ProtoType>>>();
+
+    for (const auto& val : struct_.fields()) {
+        std::string key = val.first;
+        std::shared_ptr<Value> val_ptr = std::make_shared<Value>(val.second);
+        std::shared_ptr<ProtoType> value =
+            std::make_shared<ProtoType>(ProtoType::of_value(val.second));
+        map->emplace(key, value);
+    }
+    return map;
+}
 
 // float, string, struct, array, struct(map from string to any of the above)
 
@@ -54,34 +77,6 @@ ProtoType ProtoType::of_value(Value value) {
             return ProtoType();
         }
     }
-}
-
-// NOLINTNEXTLINE(misc-no-recursion)
-Struct map_to_struct(AttributeMap dict) {
-    Struct s;
-    for (auto& key_and_value : *dict) {
-        const std::string key = key_and_value.first;
-        Value value = key_and_value.second->proto_value();
-        google::protobuf::MapPair<std::string, Value> val(key, value);
-        s.mutable_fields()->insert(val);
-    }
-
-    return s;
-}
-
-// NOLINTNEXTLINE(misc-no-recursion)
-AttributeMap struct_to_map(Struct struct_) {
-    std::shared_ptr<std::unordered_map<std::string, std::shared_ptr<ProtoType>>> map =
-        std::make_shared<std::unordered_map<std::string, std::shared_ptr<ProtoType>>>();
-
-    for (const auto& val : struct_.fields()) {
-        std::string key = val.first;
-        std::shared_ptr<Value> val_ptr = std::make_shared<Value>(val.second);
-        std::shared_ptr<ProtoType> value =
-            std::make_shared<ProtoType>(ProtoType::of_value(val.second));
-        map->emplace(key, value);
-    }
-    return map;
 }
 
 // NOLINTNEXTLINE(misc-no-recursion)
