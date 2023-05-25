2fdca644bcbda7f1d71412c592237aaa3baca048
stuqdog <stuqdog@users.noreply.github.com>
2023-05-25 18:20:56 +0000

    deploy: a984e49f4513a2f0dfc57f6f79d8cccd29989f3c

--- search/groups_0.js
+++ search/groups_0.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['classes_20related_20to_20a_20_3ctt_3erobot_3c_2ftt_3e_20representation_2e_0',['Classes related to a &lt;tt&gt;Robot&lt;/tt&gt; representation.',['../group__Robot.html',1,'']]],
-  ['classes_20related_20to_20the_20_3ctt_3ebase_3c_2ftt_3e_20component_2e_1',['Classes related to the &lt;tt&gt;Base&lt;/tt&gt; component.',['../group__Base.html',1,'']]],
-  ['classes_20related_20to_20the_20_3ctt_3eboard_3c_2ftt_3e_20component_2e_2',['Classes related to the &lt;tt&gt;Board&lt;/tt&gt; component.',['../group__Board.html',1,'']]],
-  ['classes_20related_20to_20the_20_3ctt_3ecamera_3c_2ftt_3e_20component_2e_3',['Classes related to the &lt;tt&gt;Camera&lt;/tt&gt; component.',['../group__Camera.html',1,'']]],
-  ['classes_20related_20to_20the_20_3ctt_3eencoder_3c_2ftt_3e_20component_2e_4',['Classes related to the &lt;tt&gt;Encoder&lt;/tt&gt; component.',['../group__Encoder.html',1,'']]],
-  ['classes_20related_20to_20the_20_3ctt_3egeneric_3c_2ftt_3e_20component_2e_5',['Classes related to the &lt;tt&gt;Generic&lt;/tt&gt; component.',['../group__Generic.html',1,'']]],
-  ['classes_20related_20to_20the_20_3ctt_3emotor_3c_2ftt_3e_20component_2e_6',['Classes related to the &lt;tt&gt;Motor&lt;/tt&gt; component.',['../group__Motor.html',1,'']]]
+  ['classes_20related_20to_20a_20robot_20representation_2e_0',['Classes related to a Robot representation.',['../group__Robot.html',1,'']]],
+  ['classes_20related_20to_20the_20base_20component_2e_1',['Classes related to the Base component.',['../group__Base.html',1,'']]],
+  ['classes_20related_20to_20the_20board_20component_2e_2',['Classes related to the Board component.',['../group__Board.html',1,'']]],
+  ['classes_20related_20to_20the_20camera_20component_2e_3',['Classes related to the Camera component.',['../group__Camera.html',1,'']]],
+  ['classes_20related_20to_20the_20encoder_20component_2e_4',['Classes related to the Encoder component.',['../group__Encoder.html',1,'']]],
+  ['classes_20related_20to_20the_20generic_20component_2e_5',['Classes related to the Generic component.',['../group__Generic.html',1,'']]],
+  ['classes_20related_20to_20the_20motor_20component_2e_6',['Classes related to the Motor component.',['../group__Motor.html',1,'']]]
 ];
