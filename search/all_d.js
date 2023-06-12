cf16630571e55c4a057e17b9f6198bbbc3849e59
jckras <jckras@users.noreply.github.com>
2023-06-12 19:43:31 +0000

    deploy: e6dc1cf9be5a48aa7783db84ae1b1b387aaf8bea

--- search/all_d.js
+++ search/all_d.js
@@ -1,11 +1,12 @@
 var searchData=
 [
   ['point_5fcloud_0',['point_cloud',['../structviam_1_1sdk_1_1Camera_1_1point__cloud.html',1,'viam::sdk::Camera']]],
-  ['position_1',['position',['../structposition.html',1,'position'],['../structviam_1_1sdk_1_1Encoder_1_1position.html',1,'viam::sdk::Encoder::position']]],
-  ['position_5freporting_2',['position_reporting',['../structviam_1_1sdk_1_1Motor_1_1properties.html#a6cfffefae3df6f503ead3e5f8f3b9dd9',1,'viam::sdk::Motor::properties']]],
-  ['power_5fmode_3',['power_mode',['../classviam_1_1sdk_1_1Board.html#a166386cf97ecb2e45778d96f5611ce88',1,'viam::sdk::Board']]],
-  ['power_5fpct_4',['power_pct',['../structviam_1_1sdk_1_1Motor_1_1power__status.html#a2a0673d493b18e92e14fedd7dab50d1a',1,'viam::sdk::Motor::power_status']]],
-  ['power_5fstatus_5',['power_status',['../structviam_1_1sdk_1_1Motor_1_1power__status.html',1,'viam::sdk::Motor']]],
-  ['properties_6',['properties',['../structviam_1_1sdk_1_1Camera_1_1properties.html',1,'viam::sdk::Camera::properties'],['../structviam_1_1sdk_1_1Encoder_1_1properties.html',1,'viam::sdk::Encoder::properties'],['../structviam_1_1sdk_1_1Motor_1_1properties.html',1,'viam::sdk::Motor::properties']]],
-  ['prototype_7',['ProtoType',['../classviam_1_1sdk_1_1ProtoType.html',1,'viam::sdk']]]
+  ['pose_1',['pose',['../structviam_1_1sdk_1_1GeometryConfig_1_1pose.html',1,'viam::sdk::GeometryConfig']]],
+  ['position_2',['position',['../structposition.html',1,'position'],['../structviam_1_1sdk_1_1Encoder_1_1position.html',1,'viam::sdk::Encoder::position']]],
+  ['position_5freporting_3',['position_reporting',['../structviam_1_1sdk_1_1Motor_1_1properties.html#a6cfffefae3df6f503ead3e5f8f3b9dd9',1,'viam::sdk::Motor::properties']]],
+  ['power_5fmode_4',['power_mode',['../classviam_1_1sdk_1_1Board.html#a166386cf97ecb2e45778d96f5611ce88',1,'viam::sdk::Board']]],
+  ['power_5fpct_5',['power_pct',['../structviam_1_1sdk_1_1Motor_1_1power__status.html#a2a0673d493b18e92e14fedd7dab50d1a',1,'viam::sdk::Motor::power_status']]],
+  ['power_5fstatus_6',['power_status',['../structviam_1_1sdk_1_1Motor_1_1power__status.html',1,'viam::sdk::Motor']]],
+  ['properties_7',['properties',['../structviam_1_1sdk_1_1Camera_1_1properties.html',1,'viam::sdk::Camera::properties'],['../structviam_1_1sdk_1_1Encoder_1_1properties.html',1,'viam::sdk::Encoder::properties'],['../structviam_1_1sdk_1_1Motor_1_1properties.html',1,'viam::sdk::Motor::properties']]],
+  ['prototype_8',['ProtoType',['../classviam_1_1sdk_1_1ProtoType.html',1,'viam::sdk']]]
 ];
--- search/classes_2.js
+++ search/classes_2.js
@@ -5,5 +5,6 @@ var searchData=
   ['cameraregistration_2',['CameraRegistration',['../classviam_1_1sdk_1_1CameraRegistration.html',1,'viam::sdk']]],
   ['cameraserver_3',['CameraServer',['../classviam_1_1sdk_1_1CameraServer.html',1,'viam::sdk']]],
   ['component_4',['Component',['../classviam_1_1sdk_1_1Component.html',1,'viam::sdk']]],
-  ['credentials_5',['Credentials',['../classviam_1_1sdk_1_1Credentials.html',1,'viam::sdk']]]
+  ['coordinates_5',['coordinates',['../structviam_1_1sdk_1_1GeometryConfig_1_1coordinates.html',1,'viam::sdk::GeometryConfig']]],
+  ['credentials_6',['Credentials',['../classviam_1_1sdk_1_1Credentials.html',1,'viam::sdk']]]
 ];
