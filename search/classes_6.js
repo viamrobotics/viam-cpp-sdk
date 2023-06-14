804bd87d520dd5ed3126fd826fce9a6a86c0ed76
acmorrow <acmorrow@users.noreply.github.com>
2023-06-14 17:52:58 +0000

    deploy: e97e45918b0d7936c0bc5efd6d27475e54d57460

--- search/classes_6.js
+++ search/classes_6.js
@@ -4,5 +4,7 @@ var searchData=
   ['genericclient_1',['GenericClient',['../classviam_1_1sdk_1_1GenericClient.html',1,'viam::sdk']]],
   ['genericregistration_2',['GenericRegistration',['../classviam_1_1sdk_1_1GenericRegistration.html',1,'viam::sdk']]],
   ['genericserver_3',['GenericServer',['../classviam_1_1sdk_1_1GenericServer.html',1,'viam::sdk']]],
-  ['geometryconfig_4',['GeometryConfig',['../classviam_1_1sdk_1_1GeometryConfig.html',1,'viam::sdk']]]
+  ['geometryconfig_4',['GeometryConfig',['../classviam_1_1sdk_1_1GeometryConfig.html',1,'viam::sdk']]],
+  ['get_5fhelper_5',['get_helper',['../structviam_1_1sdk_1_1prototype__details_1_1get__helper.html',1,'viam::sdk::prototype_details']]],
+  ['get_5fhelper_3c_20attributemap_20_3e_6',['get_helper&lt; AttributeMap &gt;',['../structviam_1_1sdk_1_1prototype__details_1_1get__helper_3_01AttributeMap_01_4.html',1,'viam::sdk::prototype_details']]]
 ];
