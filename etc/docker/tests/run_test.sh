271a77c177789a4bc100e80fcb9ce0ec25586d79
Andrew Morrow <andrew.morrow@viam.com>
2023-06-14 11:23:23 -0400

    UBSAN to print stack traces

--- etc/docker/tests/run_test.sh
+++ etc/docker/tests/run_test.sh
@@ -8,4 +8,4 @@ cmake .. -G Ninja -DVIAMCPPSDK_USE_DYNAMIC_PROTOS=ON \
 ninja all
 ninja install
 cd src/viam/sdk/tests
-ctest --output-on-failure
+UBSAN_OPTIONS="print_stacktrace=1" ctest --output-on-failure
