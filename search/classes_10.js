9c510eb85ef38542bf0d4d917030a1fef8a0716a
benjirewis <benjirewis@users.noreply.github.com>
2023-09-08 16:52:18 +0000

    deploy: 0a103077e257b9b9ad9028faa6cca3d303386cc9

--- search/classes_10.js
+++ search/classes_10.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['server_0',['Server',['../classviam_1_1sdk_1_1Server.html',1,'viam::sdk']]],
   ['service_1',['Service',['../classviam_1_1sdk_1_1Service.html',1,'viam::sdk']]],
-  ['sphere_2',['sphere',['../structviam_1_1sdk_1_1sphere.html',1,'viam::sdk']]],
-  ['status_3',['status',['../structviam_1_1sdk_1_1Board_1_1status.html',1,'viam::sdk::Board']]]
+  ['signalmanager_2',['SignalManager',['../classviam_1_1sdk_1_1SignalManager.html',1,'viam::sdk']]],
+  ['sphere_3',['sphere',['../structviam_1_1sdk_1_1sphere.html',1,'viam::sdk']]],
+  ['status_4',['status',['../structviam_1_1sdk_1_1Board_1_1status.html',1,'viam::sdk::Board']]]
 ];
