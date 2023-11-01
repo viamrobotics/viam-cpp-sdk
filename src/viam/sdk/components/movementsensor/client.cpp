27aa28da17a05dc8909fdcf85e0d406d81ab8e87
Abe Winter <abe-winter@users.noreply.github.com>
2023-11-01 13:42:14 -0400

    delete commented pre-stub code

--- src/viam/sdk/components/movementsensor/client.cpp
+++ src/viam/sdk/components/movementsensor/client.cpp
@@ -33,123 +33,45 @@ void throw_status(const ::grpc::Status& status) {
 using namespace viam::component::movementsensor::v1;
 
 Vector3 MovementSensorClient::get_linear_velocity(const AttributeMap& extra) {
-    // viam::component::movementsensor::v1::GetLinearVelocityRequest request;
-    // viam::component::movementsensor::v1::GetLinearVelocityResponse response;
-
-    // grpc::ClientContext ctx;
-
-    // *request.mutable_name() = this->name();
-    // *request.mutable_extra() = map_to_struct(extra);
-
-    // const grpc::Status status = stub_->GetLinearVelocity(&ctx, request, &response);
-    // if (!status.ok()) {
-    //     throw std::runtime_error(status.error_message());
-    // }
     GetLinearVelocityResponse response;
     stub_wrapper<GetLinearVelocityRequest>(this, response, extra, &MovementSensorClient::Stub::GetLinearVelocity);
     return Vector3::from_proto(response.linear_velocity());
 }
 
 Vector3 MovementSensorClient::get_angular_velocity(const AttributeMap& extra) {
-    // GetAngularVelocityRequest request;
     GetAngularVelocityResponse response;
     stub_wrapper<GetAngularVelocityRequest>(this, response, extra, &MovementSensorClient::Stub::GetAngularVelocity);
-
-    // grpc::ClientContext ctx;
-
-    // *request.mutable_name() = this->name();
-    // *request.mutable_extra() = map_to_struct(extra);
-
-    // const grpc::Status status = stub_->GetAngularVelocity(&ctx, request, &response);
-    // if (!status.ok()) {
-    //     throw std::runtime_error(status.error_message());
-    // }
     return Vector3::from_proto(response.angular_velocity());
 }
 
 MovementSensor::compassheading MovementSensorClient::get_compass_heading(
     const AttributeMap& extra) {
-    // GetCompassHeadingRequest request;
     GetCompassHeadingResponse response;
-
-    // grpc::ClientContext ctx;
-
-    // *request.mutable_name() = this->name();
-    // *request.mutable_extra() = map_to_struct(extra);
-
-    // const grpc::Status status = stub_->GetCompassHeading(&ctx, request, &response);
-    // if (!status.ok()) {
-    //     throw std::runtime_error(status.error_message());
-    // }
     stub_wrapper<GetCompassHeadingRequest>(this, response, extra, &MovementSensorClient::Stub::GetCompassHeading);
     return from_proto(response);
 }
 
 MovementSensor::orientation MovementSensorClient::get_orientation(const AttributeMap& extra) {
-    // GetOrientationRequest request;
     GetOrientationResponse response;
-
-    // grpc::ClientContext ctx;
-
-    // *request.mutable_name() = this->name();
-    // *request.mutable_extra() = map_to_struct(extra);
-
-    // const grpc::Status status = stub_->GetOrientation(&ctx, request, &response);
-    // if (!status.ok()) {
-    //     throw std::runtime_error(status.error_message());
-    // }
     stub_wrapper<GetOrientationRequest>(this, response, extra, &MovementSensorClient::Stub::GetOrientation);
     return from_proto(response.orientation());
 }
 
 MovementSensor::position MovementSensorClient::get_position(const AttributeMap& extra) {
-    // GetPositionRequest request;
     GetPositionResponse response;
-
-    // grpc::ClientContext ctx;
-
-    // *request.mutable_name() = this->name();
-    // *request.mutable_extra() = map_to_struct(extra);
-
-    // const grpc::Status status = stub_->GetPosition(&ctx, request, &response);
-    // if (!status.ok()) {
-    //     throw std::runtime_error(status.error_message());
-    // }
     stub_wrapper<GetPositionRequest>(this, response, extra, &MovementSensorClient::Stub::GetPosition);
     return from_proto(response);
 }
 
 MovementSensor::properties MovementSensorClient::get_properties(const AttributeMap& extra) {
-    // GetPropertiesRequest request;
     GetPropertiesResponse response;
-
-    // grpc::ClientContext ctx;
-
-    // *request.mutable_name() = this->name();
-    // *request.mutable_extra() = map_to_struct(extra);
-
-    // const grpc::Status status = stub_->GetProperties(&ctx, request, &response);
-    // if (!status.ok()) {
-    //     throw std::runtime_error(status.error_message());
-    // }
     stub_wrapper<GetPropertiesRequest>(this, response, extra, &MovementSensorClient::Stub::GetProperties);
     return from_proto(response);
 }
 
 std::unordered_map<std::string, float> MovementSensorClient::get_accuracy(
     const AttributeMap& extra) {
-    // GetAccuracyRequest request;
     GetAccuracyResponse response;
-
-    // grpc::ClientContext ctx;
-
-    // *request.mutable_name() = this->name();
-    // *request.mutable_extra() = map_to_struct(extra);
-
-    // const grpc::Status status = stub_->GetAccuracy(&ctx, request, &response);
-    // if (!status.ok()) {
-    //     throw std::runtime_error(status.error_message());
-    // }
     stub_wrapper<GetAccuracyRequest>(this, response, extra, &MovementSensorClient::Stub::GetAccuracy);
     std::unordered_map<std::string, float> result;
 
@@ -160,18 +82,7 @@ std::unordered_map<std::string, float> MovementSensorClient::get_accuracy(
 }
 
 Vector3 MovementSensorClient::get_linear_acceleration(const AttributeMap& extra) {
-    // GetLinearAccelerationRequest request;
     GetLinearAccelerationResponse response;
-
-    // grpc::ClientContext ctx;
-
-    // *request.mutable_name() = this->name();
-    // *request.mutable_extra() = map_to_struct(extra);
-
-    // const grpc::Status status = stub_->GetLinearAcceleration(&ctx, request, &response);
-    // if (!status.ok()) {
-    //     throw std::runtime_error(status.error_message());
-    // }
     stub_wrapper<GetLinearAccelerationRequest>(this, response, extra, &MovementSensorClient::Stub::GetLinearAcceleration);
     return Vector3::from_proto(response.linear_acceleration());
 }
@@ -192,14 +103,7 @@ AttributeMap MovementSensorClient::do_command(const AttributeMap& command) {
 
 // TODO this one didn't have a throw. is that intentional?
 std::vector<GeometryConfig> MovementSensorClient::get_geometries(const AttributeMap& extra) {
-    // viam::common::v1::GetGeometriesRequest req;
     viam::common::v1::GetGeometriesResponse resp;
-    // grpc::ClientContext ctx;
-
-    // *req.mutable_name() = this->name();
-    // *req.mutable_extra() = map_to_struct(extra);
-
-    // stub_->GetGeometries(&ctx, req, &resp);
     stub_wrapper<viam::common::v1::GetGeometriesRequest>(this, resp, extra, &MovementSensorClient::Stub::GetGeometries);
     return GeometryConfig::from_proto(resp);
 }
