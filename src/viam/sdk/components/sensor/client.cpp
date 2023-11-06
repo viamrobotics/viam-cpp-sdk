c9db156ffe0b03d46ee1834764814e24c78d2d07
Benji Rewis <benji.rewis@viam.com>
2023-11-06 20:14:30 +0000

    tests

--- src/viam/sdk/components/sensor/client.cpp
+++ src/viam/sdk/components/sensor/client.cpp
@@ -30,7 +30,7 @@ AttributeMap SensorClient::get_readings(const AttributeMap& extra) {
     AttributeMap result =
         std::make_shared<std::unordered_map<std::string, std::shared_ptr<ProtoType>>>();
     for (const auto& r : response.readings()) {
-        result->emplace(r.first, std::make_shared<ProtoType>(r.second));
+        result->emplace(std::move(r.first), std::make_shared<ProtoType>(r.second));
     }
     return result;
 }
