c0fa7bc238af5a95f2b8520397a8801fb626e0ff
Benji Rewis <benji.rewis@viam.com>
2023-11-07 19:49:51 +0000

    tests

---
+++ src/viam/sdk/tests/mocks/mock_power_sensor.cpp
@@ -0,0 +1,40 @@
+#include <viam/sdk/tests/mocks/mock_power_sensor.hpp>
+
+#include <viam/api/common/v1/common.pb.h>
+#include <viam/api/component/powersensor/v1/powersensor.grpc.pb.h>
+#include <viam/api/component/powersensor/v1/powersensor.pb.h>
+
+#include <viam/sdk/components/power_sensor/power_sensor.hpp>
+#include <viam/sdk/components/power_sensor/server.hpp>
+#include <viam/sdk/tests/test_utils.hpp>
+
+namespace viam {
+namespace sdktests {
+namespace powersensor {
+
+using namespace viam::sdk;
+
+PowerSensor::voltage MockPowerSensor::get_voltage(const AttributeMap& extra) {
+    return this->peek_voltage;
+};
+PowerSensor::current MockPowerSensor::get_current(const AttributeMap& extra) {
+    return this->peek_current;
+};
+double MockPowerSensor::get_power(const AttributeMap& extra) {
+    return this->peek_power;
+};
+AttributeMap MockPowerSensor::get_readings(const AttributeMap& extra) {
+    return fake_map();
+};
+AttributeMap MockPowerSensor::do_command(const AttributeMap& command) {
+    return command;
+};
+
+std::shared_ptr<MockPowerSensor> MockPowerSensor::get_mock_powersensor() {
+    auto powersensor = std::make_shared<MockPowerSensor>("mock_powersensor");
+    return powersensor;
+}
+
+}  // namespace powersensor
+}  // namespace sdktests
+}  // namespace viam
---
+++ src/viam/sdk/tests/mocks/mock_power_sensor.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <viam/api/common/v1/common.pb.h>
+#include <viam/api/component/powersensor/v1/powersensor.grpc.pb.h>
+#include <viam/api/component/powersensor/v1/powersensor.pb.h>
+
+#include <viam/sdk/common/proto_type.hpp>
+#include <viam/sdk/components/power_sensor/client.hpp>
+#include <viam/sdk/components/power_sensor/power_sensor.hpp>
+#include <viam/sdk/components/power_sensor/server.hpp>
+
+namespace viam {
+namespace sdktests {
+namespace powersensor {
+
+class MockPowerSensor : public sdk::PowerSensor {
+   public:
+    sdk::PowerSensor::voltage get_voltage(const sdk::AttributeMap& extra) override;
+    sdk::PowerSensor::current get_current(const sdk::AttributeMap& extra) override;
+    double get_power(const sdk::AttributeMap& extra) override;
+    sdk::AttributeMap get_readings(const sdk::AttributeMap& extra) override;
+    sdk::AttributeMap do_command(const sdk::AttributeMap& command) override;
+    static std::shared_ptr<MockPowerSensor> get_mock_powersensor();
+
+    // These variables allow the testing infra to `peek` into the mock
+    // and ensure that the correct values were passed
+    sdk::PowerSensor::voltage peek_voltage;
+    sdk::PowerSensor::current peek_current;
+    double peek_power;
+
+    MockPowerSensor(std::string name) : PowerSensor(std::move(name)){};
+};
+
+}  // namespace powersensor
+}  // namespace sdktests
+}  // namespace viam
