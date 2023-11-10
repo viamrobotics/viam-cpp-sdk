2fde2fe312a2a38384b03cde5005b732e9f4b4b7
Andrew Morrow <andrew.morrow@viam.com>
2023-11-10 15:55:39 -0500

    Convert encoder to showcase sfinae

--- src/viam/sdk/components/encoder/server.cpp
+++ src/viam/sdk/components/encoder/server.cpp
@@ -1,5 +1,6 @@
 #include <viam/sdk/components/encoder/server.hpp>
 
+#include <viam/sdk/common/service_helper.hpp>
 #include <viam/sdk/common/utils.hpp>
 #include <viam/sdk/components/encoder/encoder.hpp>
 #include <viam/sdk/config/resource.hpp>
@@ -14,132 +15,56 @@ EncoderServer::EncoderServer(std::shared_ptr<ResourceManager> manager) : Resourc
 ::grpc::Status EncoderServer::GetPosition(
     ::grpc::ServerContext* context,
     const ::viam::component::encoder::v1::GetPositionRequest* request,
-    ::viam::component::encoder::v1::GetPositionResponse* response) {
-    if (!request) {
-        return ::grpc::Status(::grpc::StatusCode::INVALID_ARGUMENT,
-                              "Called [Encoder::GetPosition] without a request");
-    };
-
-    const std::shared_ptr<Resource> rb = resource_manager()->resource(request->name());
-    if (!rb) {
-        return grpc::Status(grpc::UNKNOWN, "resource not found: " + request->name());
-    }
-
-    const std::shared_ptr<Encoder> encoder = std::dynamic_pointer_cast<Encoder>(rb);
-
-    AttributeMap extra;
-    if (request->has_extra()) {
-        extra = struct_to_map(request->extra());
-    }
-
-    const Encoder::position result =
-        encoder->get_position(extra, Encoder::from_proto(request->position_type()));
-    response->set_value(result.value);
-    response->set_position_type(Encoder::to_proto(result.type));
-
-    return ::grpc::Status();
+    ::viam::component::encoder::v1::GetPositionResponse* response) noexcept {
+    return make_service_helper<Encoder>(
+        "EncoderServer::GetPosition", this, request)([&](auto& wrapper, auto& encoder) {
+        const Encoder::position result = encoder->get_position(
+            wrapper.getExtra(), Encoder::from_proto(request->position_type()));
+        response->set_value(result.value);
+        response->set_position_type(Encoder::to_proto(result.type));
+    });
 }
 
 ::grpc::Status EncoderServer::ResetPosition(
     ::grpc::ServerContext* context,
     const ::viam::component::encoder::v1::ResetPositionRequest* request,
-    ::viam::component::encoder::v1::ResetPositionResponse* response) {
-    if (!request) {
-        return ::grpc::Status(::grpc::StatusCode::INVALID_ARGUMENT,
-                              "Called [Encoder::ResetPosition] without a request");
-    };
-
-    const std::shared_ptr<Resource> rb = resource_manager()->resource(request->name());
-    if (!rb) {
-        return grpc::Status(grpc::UNKNOWN, "resource not found: " + request->name());
-    }
-
-    const std::shared_ptr<Encoder> encoder = std::dynamic_pointer_cast<Encoder>(rb);
-
-    AttributeMap extra;
-    if (request->has_extra()) {
-        extra = struct_to_map(request->extra());
-    }
-
-    encoder->reset_position(extra);
-
-    return ::grpc::Status();
+    ::viam::component::encoder::v1::ResetPositionResponse* response) noexcept {
+    return make_service_helper<Encoder>("EncoderServer::GetPosition", this, request)(
+        [&](auto& wrapper, auto& encoder) { encoder->reset_position(wrapper.getExtra()); });
 }
 
 ::grpc::Status EncoderServer::GetProperties(
     ::grpc::ServerContext* context,
     const ::viam::component::encoder::v1::GetPropertiesRequest* request,
-    ::viam::component::encoder::v1::GetPropertiesResponse* response) {
-    if (!request) {
-        return ::grpc::Status(::grpc::StatusCode::INVALID_ARGUMENT,
-                              "Called [Encoder::GetProperties] without a request");
-    };
-
-    const std::shared_ptr<Resource> rb = resource_manager()->resource(request->name());
-    if (!rb) {
-        return grpc::Status(grpc::UNKNOWN, "resource not found: " + request->name());
-    }
-
-    const std::shared_ptr<Encoder> encoder = std::dynamic_pointer_cast<Encoder>(rb);
-
-    AttributeMap extra;
-    if (request->has_extra()) {
-        extra = struct_to_map(request->extra());
-    }
-
-    const Encoder::properties result = encoder->get_properties(extra);
-    response->set_ticks_count_supported(result.ticks_count_supported);
-    response->set_angle_degrees_supported(result.angle_degrees_supported);
-
-    return ::grpc::Status();
+    ::viam::component::encoder::v1::GetPropertiesResponse* response) noexcept {
+    return make_service_helper<Encoder>(
+        "EncoderServer::GetProperties", this, request)([&](auto& wrapper, auto& encoder) {
+        const Encoder::properties result = encoder->get_properties(wrapper.getExtra());
+        response->set_ticks_count_supported(result.ticks_count_supported);
+        response->set_angle_degrees_supported(result.angle_degrees_supported);
+    });
 }
 
 ::grpc::Status EncoderServer::GetGeometries(::grpc::ServerContext* context,
                                             const ::viam::common::v1::GetGeometriesRequest* request,
-                                            ::viam::common::v1::GetGeometriesResponse* response) {
-    if (!request) {
-        return ::grpc::Status(::grpc::StatusCode::INVALID_ARGUMENT,
-                              "Called [GetGeometries] without a request");
-    };
-
-    const std::shared_ptr<Resource> rb = resource_manager()->resource(request->name());
-    if (!rb) {
-        return grpc::Status(grpc::UNKNOWN, "resource not found: " + request->name());
-    }
-
-    AttributeMap extra;
-    if (request->has_extra()) {
-        extra = struct_to_map(request->extra());
-    }
-
-    const std::shared_ptr<Encoder> encoder = std::dynamic_pointer_cast<Encoder>(rb);
-    const std::vector<GeometryConfig> geometries = encoder->get_geometries(extra);
-    for (const auto& geometry : geometries) {
-        *response->mutable_geometries()->Add() = geometry.to_proto();
-    }
-
-    return ::grpc::Status();
+                                            ::viam::common::v1::GetGeometriesResponse* response) noexcept {
+    return make_service_helper<Encoder>(
+        "EncoderServer::GetGeometries", this, request)([&](auto& wrapper, auto& encoder) {
+        const std::vector<GeometryConfig> geometries = encoder->get_geometries(wrapper.getExtra());
+        for (const auto& geometry : geometries) {
+            *response->mutable_geometries()->Add() = geometry.to_proto();
+        }
+    });
 }
 
 ::grpc::Status EncoderServer::DoCommand(grpc::ServerContext* context,
                                         const viam::common::v1::DoCommandRequest* request,
-                                        viam::common::v1::DoCommandResponse* response) {
-    if (!request) {
-        return ::grpc::Status(::grpc::StatusCode::INVALID_ARGUMENT,
-                              "Called [Encoder::DoCommand] without a request");
-    };
-
-    const std::shared_ptr<Resource> rb = resource_manager()->resource(request->name());
-    if (!rb) {
-        return grpc::Status(grpc::UNKNOWN, "resource not found: " + request->name());
-    }
-
-    const std::shared_ptr<Encoder> encoder = std::dynamic_pointer_cast<Encoder>(rb);
-    const AttributeMap result = encoder->do_command(struct_to_map(request->command()));
-
-    *response->mutable_result() = map_to_struct(result);
-
-    return ::grpc::Status();
+                                        viam::common::v1::DoCommandResponse* response) noexcept {
+    return make_service_helper<Encoder>(
+        "EncoderServer::DoCommand", this, request)([&](auto& wrapper, auto& encoder) {
+        const AttributeMap result = encoder->do_command(struct_to_map(request->command()));
+        *response->mutable_result() = map_to_struct(result);
+    });
 }
 
 void EncoderServer::register_server(std::shared_ptr<Server> server) {
