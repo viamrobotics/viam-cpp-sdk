797df5d390339d2eadd699f30591063858e74693
Benji Rewis <benji.rewis@viam.com>
2023-11-14 22:14:41 +0000

    use ServiceHelper

--- src/viam/sdk/components/power_sensor/server.cpp
+++ src/viam/sdk/components/power_sensor/server.cpp
@@ -1,5 +1,6 @@
 #include <viam/sdk/components/power_sensor/server.hpp>
 
+#include <viam/sdk/common/service_helper.hpp>
 #include <viam/sdk/common/utils.hpp>
 #include <viam/sdk/components/power_sensor/power_sensor.hpp>
 #include <viam/sdk/config/resource.hpp>
@@ -17,79 +18,53 @@ PowerSensorServer::PowerSensorServer(std::shared_ptr<ResourceManager> manager)
 ::grpc::Status PowerSensorServer::GetVoltage(::grpc::ServerContext* context,
                                              const GetVoltageRequest* request,
                                              GetVoltageResponse* response) {
-    const auto status = server_wrapper<PowerSensor>(request);
-    if (!status.ok()) {
-        return status.status;
-    }
-
-    PowerSensor::voltage result = status.powersensor->get_voltage(status.extra);
-    *response = PowerSensor::to_proto(result);
-
-    return ::grpc::Status();
+    return make_service_helper<PowerSensor>(
+        "PowerSensorServer::GetVoltage", this, request)([&](auto& helper, auto& powersensor) {
+        PowerSensor::voltage result = powersensor->get_voltage(helper.getExtra());
+        *response = PowerSensor::to_proto(result);
+    });
 }
 
 ::grpc::Status PowerSensorServer::GetCurrent(::grpc::ServerContext* context,
                                              const GetCurrentRequest* request,
                                              GetCurrentResponse* response) {
-    const auto status = server_wrapper<PowerSensor>(request);
-    if (!status.ok()) {
-        return status.status;
-    }
-
-    PowerSensor::current result = status.powersensor->get_current(status.extra);
-    *response = PowerSensor::to_proto(result);
-
-    return ::grpc::Status();
+    return make_service_helper<PowerSensor>(
+        "PowerSensorServer::GetCurrent", this, request)([&](auto& helper, auto& powersensor) {
+        PowerSensor::current result = powersensor->get_current(helper.getExtra());
+        *response = PowerSensor::to_proto(result);
+    });
 }
 
 ::grpc::Status PowerSensorServer::GetPower(::grpc::ServerContext* context,
                                            const GetPowerRequest* request,
                                            GetPowerResponse* response) {
-    const auto status = server_wrapper<PowerSensor>(request);
-    if (!status.ok()) {
-        return status.status;
-    }
-
-    double watts = status.powersensor->get_power(status.extra);
-    response->set_watts(watts);
-
-    return ::grpc::Status();
+    return make_service_helper<PowerSensor>(
+        "PowerSensorServer::GetPower", this, request)([&](auto& helper, auto& powersensor) {
+        double watts = powersensor->get_power(helper.getExtra());
+        response->set_watts(watts);
+    });
 }
 
 ::grpc::Status PowerSensorServer::GetReadings(::grpc::ServerContext* context,
                                               const viam::common::v1::GetReadingsRequest* request,
                                               viam::common::v1::GetReadingsResponse* response) {
-    const auto status = server_wrapper<PowerSensor>(request);
-    if (!status.ok()) {
-        return status.status;
-    }
-
-    const AttributeMap result = status.powersensor->get_readings(status.extra);
-    for (const auto& r : *result) {
-        response->mutable_readings()->insert({std::move(r.first), r.second->proto_value()});
-    }
-    return ::grpc::Status();
+    return make_service_helper<PowerSensor>(
+        "PowerSensorServer::GetReadings", this, request)([&](auto& helper, auto& powersensor) {
+        const AttributeMap result = powersensor->get_readings(helper.getExtra());
+        for (const auto& r : *result) {
+            response->mutable_readings()->insert({std::move(r.first), r.second->proto_value()});
+        }
+    });
 }
 
 ::grpc::Status PowerSensorServer::DoCommand(grpc::ServerContext* context,
                                             const viam::common::v1::DoCommandRequest* request,
                                             viam::common::v1::DoCommandResponse* response) {
-    if (!request) {
-        return ::grpc::Status(::grpc::StatusCode::INVALID_ARGUMENT,
-                              "Called [PowerSensor::DoCommand] without a request");
-    };
-
-    auto rb = resource_manager()->resource(request->name());
-    if (!rb) {
-        return grpc::Status(grpc::UNKNOWN, "resource not found: " + request->name());
-    }
-
-    const std::shared_ptr<PowerSensor> sensor = std::dynamic_pointer_cast<PowerSensor>(rb);
-    const AttributeMap result = sensor->do_command(struct_to_map(request->command()));
-
-    *response->mutable_result() = map_to_struct(result);
-
-    return ::grpc::Status();
+    return make_service_helper<PowerSensor>(
+        "PowerSensorServer::DoCommand", this, request)([&](auto& helper, auto& powersensor) {
+        const AttributeMap result = powersensor->do_command(struct_to_map(request->command()));
+        *response->mutable_result() = map_to_struct(result);
+    });
 }
 
 void PowerSensorServer::register_server(std::shared_ptr<Server> server) {
--- src/viam/sdk/components/power_sensor/server.hpp
+++ src/viam/sdk/components/power_sensor/server.hpp
@@ -43,39 +43,6 @@ class PowerSensorServer : public ResourceServer, public PowerSensorService::Serv
                              viam::common::v1::DoCommandResponse* response) override;
 
     void register_server(std::shared_ptr<Server> server) override;
-
-   private:
-    template <typename Cls>
-    struct ServerWrapperResult {
-        ::grpc::Status status;
-        AttributeMap extra;
-        std::shared_ptr<Cls> powersensor;
-
-        bool ok() const {
-            return status.ok();
-        }
-    };
-
-    // wrapper template for boilerplate
-    template <typename Cls, typename Req>
-    ServerWrapperResult<Cls> server_wrapper(const Req* request) {
-        if (!request) {
-            auto status = ::grpc::Status(::grpc::StatusCode::INVALID_ARGUMENT,
-                                         "Called PowerSensorServer method without a request");
-            return ServerWrapperResult<Cls>{status};
-        };
-        const std::shared_ptr<Resource> rb = resource_manager()->resource(request->name());
-        if (!rb) {
-            auto status = grpc::Status(grpc::UNKNOWN, "resource not found: " + request->name());
-            return ServerWrapperResult<Cls>{status};
-        }
-        ServerWrapperResult<Cls> ret;
-        ret.powersensor = std::dynamic_pointer_cast<Cls>(rb);
-        if (request->has_extra()) {
-            ret.extra = struct_to_map(request->extra());
-        }
-        return ret;
-    }
 };
 
 }  // namespace sdk
