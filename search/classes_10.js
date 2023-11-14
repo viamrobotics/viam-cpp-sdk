d4d1a49ceaec6e6ebe92496bb6f7cdb38097e6a4
acmorrow <acmorrow@users.noreply.github.com>
2023-11-14 16:19:22 +0000

    deploy: 229d9505b8ac180813c21833cda8d0060cbf0515

--- search/classes_10.js
+++ search/classes_10.js
@@ -2,11 +2,13 @@ var searchData=
 [
   ['server_0',['Server',['../classviam_1_1sdk_1_1Server.html',1,'viam::sdk']]],
   ['service_1',['Service',['../classviam_1_1sdk_1_1Service.html',1,'viam::sdk']]],
-  ['signalmanager_2',['SignalManager',['../classviam_1_1sdk_1_1SignalManager.html',1,'viam::sdk']]],
-  ['sphere_3',['sphere',['../structviam_1_1sdk_1_1sphere.html',1,'viam::sdk']]],
-  ['status_4',['status',['../structviam_1_1sdk_1_1Board_1_1status.html',1,'viam::sdk::Board']]],
-  ['summation_5',['Summation',['../classSummation.html',1,'']]],
-  ['summationclient_6',['SummationClient',['../classSummationClient.html',1,'']]],
-  ['summationregistration_7',['SummationRegistration',['../classSummationRegistration.html',1,'']]],
-  ['summationserver_8',['SummationServer',['../classSummationServer.html',1,'']]]
+  ['servicehelper_2',['ServiceHelper',['../classviam_1_1sdk_1_1ServiceHelper.html',1,'viam::sdk']]],
+  ['servicehelperbase_3',['ServiceHelperBase',['../classviam_1_1sdk_1_1ServiceHelperBase.html',1,'viam::sdk']]],
+  ['signalmanager_4',['SignalManager',['../classviam_1_1sdk_1_1SignalManager.html',1,'viam::sdk']]],
+  ['sphere_5',['sphere',['../structviam_1_1sdk_1_1sphere.html',1,'viam::sdk']]],
+  ['status_6',['status',['../structviam_1_1sdk_1_1Board_1_1status.html',1,'viam::sdk::Board']]],
+  ['summation_7',['Summation',['../classSummation.html',1,'']]],
+  ['summationclient_8',['SummationClient',['../classSummationClient.html',1,'']]],
+  ['summationregistration_9',['SummationRegistration',['../classSummationRegistration.html',1,'']]],
+  ['summationserver_10',['SummationServer',['../classSummationServer.html',1,'']]]
 ];
