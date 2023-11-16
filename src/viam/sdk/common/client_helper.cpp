600415d957ba774481ac4ae2ea63a203bd91ed07
Andrew Morrow <andrew.morrow@viam.com>
2023-11-16 10:42:17 -0500

    clang format

--- src/viam/sdk/common/client_helper.cpp
+++ src/viam/sdk/common/client_helper.cpp
@@ -10,8 +10,9 @@ namespace sdk {
 namespace client_helper_details {
 
 [[noreturn]] void errorHandlerReturnedUnexpectedly(const ::grpc::Status& status) noexcept {
-  BOOST_LOG_TRIVIAL(fatal) << "ClientHelper error handler callback returned instead of throwing: " << status.error_message() << '(' << status.error_details() << ')';
-  std::abort();
+    BOOST_LOG_TRIVIAL(fatal) << "ClientHelper error handler callback returned instead of throwing: "
+                             << status.error_message() << '(' << status.error_details() << ')';
+    std::abort();
 }
 
 }  // namespace client_helper_details
