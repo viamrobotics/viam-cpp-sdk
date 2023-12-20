a312cb7a8c466489e93162a65b02212f6ecce14b
stuqdog <stuqdog@users.noreply.github.com>
2023-12-20 15:33:14 +0000

    deploy: 5db32ae3d1c9b7f8cec48c97487105c4c2070281

--- search/classes_2.js
+++ search/classes_2.js
@@ -5,12 +5,13 @@ var searchData=
   ['cameraregistration_2',['CameraRegistration',['../classviam_1_1sdk_1_1CameraRegistration.html',1,'viam::sdk']]],
   ['cameraserver_3',['CameraServer',['../classviam_1_1sdk_1_1CameraServer.html',1,'viam::sdk']]],
   ['capsule_4',['capsule',['../structviam_1_1sdk_1_1capsule.html',1,'viam::sdk']]],
-  ['clienthelper_5',['ClientHelper',['../classviam_1_1sdk_1_1ClientHelper.html',1,'viam::sdk']]],
-  ['collision_5fspecification_6',['collision_specification',['../structviam_1_1sdk_1_1Motion_1_1collision__specification.html',1,'viam::sdk::Motion']]],
-  ['compassheading_7',['compassheading',['../structviam_1_1sdk_1_1MovementSensor_1_1compassheading.html',1,'viam::sdk::MovementSensor']]],
-  ['component_8',['Component',['../classviam_1_1sdk_1_1Component.html',1,'viam::sdk']]],
-  ['constraints_9',['constraints',['../structviam_1_1sdk_1_1Motion_1_1constraints.html',1,'viam::sdk::Motion']]],
-  ['coordinates_10',['coordinates',['../structviam_1_1sdk_1_1coordinates.html',1,'viam::sdk']]],
-  ['credentials_11',['Credentials',['../classviam_1_1sdk_1_1Credentials.html',1,'viam::sdk']]],
-  ['current_12',['current',['../structviam_1_1sdk_1_1PowerSensor_1_1current.html',1,'viam::sdk::PowerSensor']]]
+  ['clientcontext_5',['ClientContext',['../classviam_1_1sdk_1_1ClientContext.html',1,'viam::sdk']]],
+  ['clienthelper_6',['ClientHelper',['../classviam_1_1sdk_1_1ClientHelper.html',1,'viam::sdk']]],
+  ['collision_5fspecification_7',['collision_specification',['../structviam_1_1sdk_1_1Motion_1_1collision__specification.html',1,'viam::sdk::Motion']]],
+  ['compassheading_8',['compassheading',['../structviam_1_1sdk_1_1MovementSensor_1_1compassheading.html',1,'viam::sdk::MovementSensor']]],
+  ['component_9',['Component',['../classviam_1_1sdk_1_1Component.html',1,'viam::sdk']]],
+  ['constraints_10',['constraints',['../structviam_1_1sdk_1_1Motion_1_1constraints.html',1,'viam::sdk::Motion']]],
+  ['coordinates_11',['coordinates',['../structviam_1_1sdk_1_1coordinates.html',1,'viam::sdk']]],
+  ['credentials_12',['Credentials',['../classviam_1_1sdk_1_1Credentials.html',1,'viam::sdk']]],
+  ['current_13',['current',['../structviam_1_1sdk_1_1PowerSensor_1_1current.html',1,'viam::sdk::PowerSensor']]]
 ];
