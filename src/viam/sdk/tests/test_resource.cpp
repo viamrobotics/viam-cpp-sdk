b8a18a097a8ccfe77cd7f8c8f292119962238712
purplenicole730 <jihyun.nicole.jung@gmail.com>
2024-01-10 10:09:11 -0500

    test api setting

---
+++ src/viam/sdk/tests/test_resource.cpp
@@ -0,0 +1,26 @@
+#define BOOST_TEST_MODULE test module test_resource
+#include <boost/test/included/unit_test.hpp>
+
+#include <viam/sdk/config/resource.hpp>
+#include <viam/sdk/resource/resource_api.hpp>
+
+namespace viam {
+namespace sdktests {
+
+using namespace viam::sdk;
+
+BOOST_AUTO_TEST_CASE(test_api) {
+    API api;
+    api.set_namespace("ns");
+    BOOST_CHECK_EQUAL(api.type_namespace(), "ns");
+    api.set_resource_type("component");
+    BOOST_CHECK_EQUAL(api.resource_type(), "component");
+    api.set_resource_subtype("st");
+    BOOST_CHECK_EQUAL(api.resource_subtype(), "st");
+    BOOST_CHECK_EQUAL(api.to_string(), "ns:component:st");
+    BOOST_CHECK(!api.is_service_type());
+    BOOST_CHECK(api.is_component_type());
+}
+
+}  // namespace sdktests
+}  // namespace viam
