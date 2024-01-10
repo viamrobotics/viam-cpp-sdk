0d1faea2011223b8b6e1ed817aa4ba319c331472
purplenicole730 <jihyun.nicole.jung@gmail.com>
2024-01-10 13:46:30 -0500

    number all test examples

--- src/viam/sdk/tests/test_resource.cpp
+++ src/viam/sdk/tests/test_resource.cpp
@@ -10,16 +10,16 @@ namespace sdktests {
 using namespace viam::sdk;
 
 BOOST_AUTO_TEST_CASE(test_api) {
-    API api;
-    api.set_namespace("ns");
-    BOOST_CHECK_EQUAL(api.type_namespace(), "ns");
-    api.set_resource_type("component");
-    BOOST_CHECK_EQUAL(api.resource_type(), "component");
-    api.set_resource_subtype("st");
-    BOOST_CHECK_EQUAL(api.resource_subtype(), "st");
-    BOOST_CHECK_EQUAL(api.to_string(), "ns:component:st");
-    BOOST_CHECK(!api.is_service_type());
-    BOOST_CHECK(api.is_component_type());
+    API api1;
+    api1.set_namespace("ns");
+    BOOST_CHECK_EQUAL(api1.type_namespace(), "ns");
+    api1.set_resource_type("component");
+    BOOST_CHECK_EQUAL(api1.resource_type(), "component");
+    api1.set_resource_subtype("st");
+    BOOST_CHECK_EQUAL(api1.resource_subtype(), "st");
+    BOOST_CHECK_EQUAL(api1.to_string(), "ns:component:st");
+    BOOST_CHECK(!api1.is_service_type());
+    BOOST_CHECK(api1.is_component_type());
 
     API api2 = API::from_string("ns:service:st");
     BOOST_CHECK_EQUAL(api2.to_string(), "ns:service:st");
@@ -32,12 +32,12 @@ BOOST_AUTO_TEST_CASE(test_api) {
 }
 
 BOOST_AUTO_TEST_CASE(test_name) {
-    Name name(API::from_string("ns:service:st"), "remote", "name");
-    BOOST_CHECK_EQUAL(name.api().to_string(), "ns:service:st");
-    BOOST_CHECK_EQUAL(name.remote_name(), "remote");
-    BOOST_CHECK_EQUAL(name.name(), "name");
-    BOOST_CHECK_EQUAL(name.to_string(), "ns:service:st/remote:name");
-    BOOST_CHECK_EQUAL(name.short_name(), "remote:name");
+    Name name1(API::from_string("ns:service:st"), "remote", "name");
+    BOOST_CHECK_EQUAL(name1.api().to_string(), "ns:service:st");
+    BOOST_CHECK_EQUAL(name1.remote_name(), "remote");
+    BOOST_CHECK_EQUAL(name1.name(), "name");
+    BOOST_CHECK_EQUAL(name1.to_string(), "ns:service:st/remote:name");
+    BOOST_CHECK_EQUAL(name1.short_name(), "remote:name");
 
     Name name2 = Name::from_string("ns:component:st/name");
     BOOST_CHECK_EQUAL(name2.api().to_string(), "ns:component:st");
