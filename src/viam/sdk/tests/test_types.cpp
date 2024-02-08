044e34d6a7ae3adb63d7bcb082ba64d9b1b06803
purplenicole730 <jihyun.nicole.jung@gmail.com>
2024-02-08 12:05:52 -0500

    test discovery to and from proto methods

--- src/viam/sdk/tests/test_types.cpp
+++ src/viam/sdk/tests/test_types.cpp
@@ -1,12 +1,14 @@
 #include <memory>
 #define BOOST_TEST_MODULE test module test_types
 
+#include <cctype>
 #include <unordered_map>
 
 #include <boost/test/included/unit_test.hpp>
 
 #include <viam/sdk/common/proto_type.hpp>
 #include <viam/sdk/config/resource.hpp>
+#include <viam/sdk/robot/client.hpp>
 #include <viam/sdk/tests/test_utils.hpp>
 
 namespace viam {
@@ -109,6 +111,52 @@ BOOST_AUTO_TEST_CASE(test_prototype_map_conversion) {
 
     ProtoType round_trip_proto(round_trip2);
     BOOST_CHECK(round_trip_proto == map);
+}
+
+std::string remove_whitespace(std::string s) {
+    s.erase(std::remove_if(s.begin(), s.end(), ::isspace), s.end());
+    return s;
+}
+
+BOOST_AUTO_TEST_CASE(test_discovery_query) {
+    RobotClient::discovery_query query;
+    query.subtype = "subtype";
+    query.model = "model";
+
+    BOOST_CHECK_EQUAL(query.subtype, "subtype");
+    BOOST_CHECK_EQUAL(query.model, "model");
+
+    RobotClient::discovery_query from_proto =
+        RobotClient::discovery_query::from_proto(query.to_proto());
+    BOOST_CHECK_EQUAL(query.subtype, from_proto.subtype);
+    BOOST_CHECK_EQUAL(query.model, query.model);
+    BOOST_CHECK(query == from_proto);
+}
+
+BOOST_AUTO_TEST_CASE(test_discovery) {
+    RobotClient::discovery discovery;
+
+    RobotClient::discovery_query query;
+    query.subtype = "subtype";
+    query.model = "model";
+    discovery.query = query;
+
+    AttributeMap results =
+        std::make_shared<std::unordered_map<std::string, std::shared_ptr<ProtoType>>>();
+    google::protobuf::Value v;
+    *v.mutable_string_value() = "bar";
+    google::protobuf::MapPair<std::string, std::shared_ptr<ProtoType>> pair(
+        std::move("foo"), std::make_shared<ProtoType>(v));
+    results->emplace(pair);
+    discovery.results = results;
+
+    BOOST_CHECK_EQUAL(discovery.query.subtype, "subtype");
+    BOOST_CHECK_EQUAL(discovery.query.model, "model");
+    std::string results_str =
+        remove_whitespace(map_to_struct(discovery.results).SerializeAsString());
+    BOOST_CHECK_EQUAL(results_str, "foobar");
+
+    // RobotClient::
 }
 
 BOOST_AUTO_TEST_SUITE_END()
