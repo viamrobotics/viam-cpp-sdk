9343e984fd96c66337623faddd18e80fab8b284f
stuqdog <stuqdog@users.noreply.github.com>
2024-05-20 19:04:36 +0000

    deploy: 5def8e00b6a0db34d6d684e370a41d4918f7ac88

--- search/classes_6.js
+++ search/classes_6.js
@@ -6,7 +6,7 @@ var searchData=
   ['genericservice_3',['GenericService',['../classviam_1_1sdk_1_1GenericService.html',1,'viam::sdk']]],
   ['genericserviceclient_4',['GenericServiceClient',['../classviam_1_1sdk_1_1impl_1_1GenericServiceClient.html',1,'viam::sdk::impl']]],
   ['genericserviceserver_5',['GenericServiceServer',['../classviam_1_1sdk_1_1impl_1_1GenericServiceServer.html',1,'viam::sdk::impl']]],
-  ['geo_5fobstacle_6',['geo_obstacle',['../structviam_1_1sdk_1_1geo__obstacle.html',1,'viam::sdk']]],
+  ['geo_5fgeometry_6',['geo_geometry',['../structviam_1_1sdk_1_1geo__geometry.html',1,'viam::sdk']]],
   ['geo_5fpoint_7',['geo_point',['../structviam_1_1sdk_1_1geo__point.html',1,'viam::sdk']]],
   ['geometries_5fin_5fframe_8',['geometries_in_frame',['../structviam_1_1sdk_1_1WorldState_1_1geometries__in__frame.html',1,'viam::sdk::WorldState']]],
   ['geometryconfig_9',['GeometryConfig',['../classviam_1_1sdk_1_1GeometryConfig.html',1,'viam::sdk']]],
