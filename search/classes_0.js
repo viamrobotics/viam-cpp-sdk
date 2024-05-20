032d927e41faa81f0e5d29abe3081dbdc7d0634a
penguinland <penguinland@users.noreply.github.com>
2024-05-20 18:21:22 +0000

    deploy: ea566a03750e093baa0971d472be49fb97e3dd3c

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['allowed_5fframe_5fcollisions_0',['allowed_frame_collisions',['../structviam_1_1sdk_1_1Motion_1_1collision__specification_1_1allowed__frame__collisions.html',1,'viam::sdk::Motion::collision_specification']]],
-  ['api_1',['API',['../classviam_1_1sdk_1_1API.html',1,'viam::sdk']]],
-  ['apitype_2',['APIType',['../classviam_1_1sdk_1_1APIType.html',1,'viam::sdk']]],
-  ['axis_5fangles_3',['axis_angles',['../structviam_1_1sdk_1_1axis__angles.html',1,'viam::sdk']]]
+  ['analog_5fresponse_1',['analog_response',['../structviam_1_1sdk_1_1Board_1_1analog__response.html',1,'viam::sdk::Board']]],
+  ['api_2',['API',['../classviam_1_1sdk_1_1API.html',1,'viam::sdk']]],
+  ['apitype_3',['APIType',['../classviam_1_1sdk_1_1APIType.html',1,'viam::sdk']]],
+  ['axis_5fangles_4',['axis_angles',['../structviam_1_1sdk_1_1axis__angles.html',1,'viam::sdk']]]
 ];
