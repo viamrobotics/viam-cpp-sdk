3907d004244530dac5d74228e55151a5d5f72c32
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2024-06-17 14:10:47 -0400

    add arm_server

---
+++ src/viam/sdk/components/private/arm_server.cpp
@@ -0,0 +1,127 @@
+#include <viam/sdk/components/private/arm_server.hpp>
+
+#include <viam/sdk/common/service_helper.hpp>
+
+namespace viam {
+namespace sdk {
+namespace impl {
+
+ArmServer::ArmServer(std::shared_ptr<ResourceManager> manager)
+    : ResourceServer(std::move(manager)) {}
+
+::grpc::Status ArmServer::GetEndPosition(
+    ::grpc::ServerContext* context,
+    const ::viam::component::arm::v1::GetEndPositionRequest* request,
+    ::viam::component::arm::v1::GetEndPositionResponse* response) noexcept {
+    return make_service_helper<Arm>(
+        "ArmServer::GetEndPosition", this, request)([&](auto& helper, auto& arm) {
+        const pose p = arm->get_end_position(helper.getExtra());
+        *response->mutable_pose() = p.to_proto();
+    });
+}
+
+::grpc::Status ArmServer::MoveToPosition(
+    ::grpc::ServerContext* context,
+    const ::viam::component::arm::v1::MoveToPositionRequest* request,
+    ::viam::component::arm::v1::MoveToPositionResponse* response) noexcept {
+    return make_service_helper<Arm>(
+        "ArmServer::MoveToPosition", this, request)([&](auto& helper, auto& arm) {
+        arm->move_to_position(pose::from_proto(request->to()), helper.getExtra());
+    });
+}
+
+::grpc::Status ArmServer::GetJointPositions(
+    ::grpc::ServerContext* context,
+    const ::viam::component::arm::v1::GetJointPositionsRequest* request,
+    ::viam::component::arm::v1::GetJointPositionsResponse* response) noexcept {
+    return make_service_helper<Arm>(
+        "ArmServer::GetJointPositions", this, request)([&](auto& helper, auto& arm) {
+        const std::vector<double> positions = arm->get_joint_positions(helper.getExtra());
+        response->mutable_positions()->mutable_values()->Assign(positions.begin(), positions.end());
+    });
+}
+
+::grpc::Status ArmServer::MoveToJointPositions(
+    ::grpc::ServerContext* context,
+    const ::viam::component::arm::v1::MoveToJointPositionsRequest* request,
+    ::viam::component::arm::v1::MoveToJointPositionsResponse* response) noexcept {
+    return make_service_helper<Arm>(
+        "ArmServer::MoveToJointPositions", this, request)([&](auto& helper, auto& arm) {
+        arm->move_to_joint_positions(
+            {request->positions().values().begin(), request->positions().values().end()},
+            helper.getExtra());
+    });
+}
+
+::grpc::Status ArmServer::Stop(::grpc::ServerContext* context,
+                               const ::viam::component::arm::v1::StopRequest* request,
+                               ::viam::component::arm::v1::StopResponse* response) noexcept {
+    return make_service_helper<Arm>("ArmServer::Stop", this, request)(
+        [&](auto& helper, auto& arm) { arm->stop(helper.getExtra()); });
+}
+
+::grpc::Status ArmServer::IsMoving(
+    ::grpc::ServerContext* context,
+    const ::viam::component::arm::v1::IsMovingRequest* request,
+    ::viam::component::arm::v1::IsMovingResponse* response) noexcept {
+    return make_service_helper<Arm>("ArmServer::IsMoving", this, request)(
+        [&](auto&, auto& arm) { response->set_is_moving(arm->is_moving()); });
+}
+
+::grpc::Status ArmServer::DoCommand(::grpc::ServerContext* context,
+                                    const ::viam::common::v1::DoCommandRequest* request,
+                                    ::viam::common::v1::DoCommandResponse* response) noexcept {
+    return make_service_helper<Arm>("ArmServer::DoCommand", this, request)([&](auto&, auto& arm) {
+        const AttributeMap result = arm->do_command(struct_to_map(request->command()));
+        *response->mutable_result() = map_to_struct(result);
+    });
+}
+
+::grpc::Status ArmServer::GetKinematics(
+    ::grpc::ServerContext* context,
+    const ::viam::common::v1::GetKinematicsRequest* request,
+    ::viam::common::v1::GetKinematicsResponse* response) noexcept {
+    return make_service_helper<Arm>(
+        "ArmServer::GetKinematics", this, request)([&](auto& helper, auto& arm) {
+        const Arm::KinematicsData result = arm->get_kinematics(helper.getExtra());
+
+        struct Visitor {
+            using FileFormat = common::v1::KinematicsFileFormat;
+            auto operator()(const Arm::KinematicsDataUnspecified&) const noexcept {
+                return FileFormat::KINEMATICS_FILE_FORMAT_UNSPECIFIED;
+            }
+
+            auto operator()(const Arm::KinematicsDataSVA&) const noexcept {
+                return FileFormat::KINEMATICS_FILE_FORMAT_SVA;
+            }
+
+            auto operator()(const Arm::KinematicsDataURDF&) const noexcept {
+                return FileFormat::KINEMATICS_FILE_FORMAT_URDF;
+            }
+        } visitor;
+
+        boost::apply_visitor(
+            [&](const auto& v) {
+                response->set_format(visitor(v));
+                response->mutable_kinematics_data()->assign(v.bytes.begin(), v.bytes.end());
+            },
+            result);
+    });
+}
+
+::grpc::Status ArmServer::GetGeometries(
+    ::grpc::ServerContext* context,
+    const ::viam::common::v1::GetGeometriesRequest* request,
+    ::viam::common::v1::GetGeometriesResponse* response) noexcept {
+    return make_service_helper<Arm>(
+        "ArmServer::GetGeometries", this, request)([&](auto& helper, auto& arm) {
+        const std::vector<GeometryConfig> geometries = arm->get_geometries(helper.getExtra());
+        for (const auto& geometry : geometries) {
+            *response->mutable_geometries()->Add() = geometry.to_proto();
+        }
+    });
+}
+
+}  // namespace impl
+}  // namespace sdk
+}  // namespace viam
---
+++ src/viam/sdk/components/private/arm_server.hpp
@@ -0,0 +1,73 @@
+/// @file components/private/arm_server.hpp
+///
+/// @brief Implements a gRPC server for the `Arm` component
+#pragma once
+
+#include <viam/api/component/arm/v1/arm.grpc.pb.h>
+#include <viam/api/component/arm/v1/arm.pb.h>
+
+#include <viam/sdk/components/arm.hpp>
+#include <viam/sdk/resource/resource_manager.hpp>
+#include <viam/sdk/resource/resource_server_base.hpp>
+
+namespace viam {
+namespace sdk {
+namespace impl {
+
+/// @class ArmServer
+/// @brief gRPC server implementation of an `Arm` component.
+/// @ingroup Arm
+class ArmServer : public ResourceServer, public viam::component::arm::v1::ArmService::Service {
+   public:
+    using interface_type = Arm;
+    using service_type = component::arm::v1::ArmService::Service;
+
+    explicit ArmServer(std::shared_ptr<ResourceManager> manager);
+
+    ::grpc::Status GetEndPosition(
+        ::grpc::ServerContext* context,
+        const ::viam::component::arm::v1::GetEndPositionRequest* request,
+        ::viam::component::arm::v1::GetEndPositionResponse* response) noexcept override;
+
+    ::grpc::Status MoveToPosition(
+        ::grpc::ServerContext* context,
+        const ::viam::component::arm::v1::MoveToPositionRequest* request,
+        ::viam::component::arm::v1::MoveToPositionResponse* response) noexcept override;
+
+    ::grpc::Status GetJointPositions(
+        ::grpc::ServerContext* context,
+        const ::viam::component::arm::v1::GetJointPositionsRequest* request,
+        ::viam::component::arm::v1::GetJointPositionsResponse* response) noexcept override;
+
+    ::grpc::Status MoveToJointPositions(
+        ::grpc::ServerContext* context,
+        const ::viam::component::arm::v1::MoveToJointPositionsRequest* request,
+        ::viam::component::arm::v1::MoveToJointPositionsResponse* response) noexcept override;
+
+    ::grpc::Status Stop(::grpc::ServerContext* context,
+                        const ::viam::component::arm::v1::StopRequest* request,
+                        ::viam::component::arm::v1::StopResponse* response) noexcept override;
+
+    ::grpc::Status IsMoving(
+        ::grpc::ServerContext* context,
+        const ::viam::component::arm::v1::IsMovingRequest* request,
+        ::viam::component::arm::v1::IsMovingResponse* response) noexcept override;
+
+    ::grpc::Status DoCommand(::grpc::ServerContext* context,
+                             const ::viam::common::v1::DoCommandRequest* request,
+                             ::viam::common::v1::DoCommandResponse* response) noexcept override;
+
+    ::grpc::Status GetKinematics(
+        ::grpc::ServerContext* context,
+        const ::viam::common::v1::GetKinematicsRequest* request,
+        ::viam::common::v1::GetKinematicsResponse* response) noexcept override;
+
+    ::grpc::Status GetGeometries(
+        ::grpc::ServerContext* context,
+        const ::viam::common::v1::GetGeometriesRequest* request,
+        ::viam::common::v1::GetGeometriesResponse* response) noexcept override;
+};
+
+}  // namespace impl
+}  // namespace sdk
+}  // namespace viam
