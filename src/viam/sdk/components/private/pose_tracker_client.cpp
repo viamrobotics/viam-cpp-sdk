15939f75f6376799aebbad0de3bfffec48255067
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2024-06-26 11:37:23 -0400

    add pose tracker client

---
+++ src/viam/sdk/components/private/pose_tracker_client.cpp
@@ -0,0 +1,52 @@
+#include <viam/sdk/components/private/pose_tracker_client.hpp>
+
+#include <viam/api/common/v1/common.pb.h>
+#include <viam/api/component/posetracker/v1/pose_tracker.grpc.pb.h>
+#include <viam/api/component/posetracker/v1/pose_tracker.pb.h>
+
+#include "pose_tracker_client.hpp"
+#include <viam/sdk/common/client_helper.hpp>
+
+namespace viam {
+namespace sdk {
+namespace impl {
+
+PoseTrackerClient::PoseTrackerClient(std::string name, std::shared_ptr<grpc::Channel> channel)
+    : PoseTracker(std::move(name)),
+      stub_(viam::component::posetracker::v1::PoseTrackerService::NewStub(channel)),
+      channel_(std::move(channel)) {}
+
+std::unordered_map<std::string, pose_in_frame> PoseTrackerClient::get_poses(
+    const std::string& tracker_name,
+    const std::vector<std::string>& body_names,
+    const AttributeMap& extra) {
+    return make_client_helper(this, *stub_, &StubType::GetPoses)
+        .with([&](viam::component::posetracker::v1::GetPosesRequest& request) {
+            *request.mutable_name() = tracker_name;
+            *request.mutable_body_names() = {body_names.begin(), body_names.end()};
+        })
+        .invoke([](const viam::component::posetracker::v1::GetPosesResponse& response) {
+            std::unordered_map<std::string, pose_in_frame> result;
+
+            for (const auto& pair : response.body_poses())
+                result.emplace(pair.first, pose_in_frame::from_proto(pair.second));
+
+            return result;
+        });
+}
+
+std::vector<GeometryConfig> PoseTrackerClient::get_geometries(const AttributeMap& extra) {
+    return make_client_helper(this, *stub_, &StubType::GetGeometries)
+        .with(extra)
+        .invoke([](auto& response) { return GeometryConfig::from_proto(response); });
+}
+
+AttributeMap PoseTrackerClient::do_command(const AttributeMap& command) {
+    return make_client_helper(this, *stub_, &StubType::DoCommand)
+        .with([&](auto& request) { *request.mutable_command() = map_to_struct(command); })
+        .invoke([](auto& response) { return struct_to_map(response.result()); });
+}
+
+}  // namespace impl
+}  // namespace sdk
+}  // namespace viam
