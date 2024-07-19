dd59a676b2627c50e40c6b72d43c5ca53d80b8ee
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2024-07-19 15:08:24 -0400

    update signal manager to allow handlers for async signals

--- src/viam/sdk/module/signal_manager.cpp
+++ src/viam/sdk/module/signal_manager.cpp
@@ -3,19 +3,58 @@
 #include <csignal>
 #include <pthread.h>
 
+#include <array>
+#include <tuple>
+
+#include <boost/mp11/tuple.hpp>
+
 namespace viam {
 namespace sdk {
 
-SignalManager::SignalManager() {
+using signal_map = SignalManager::signal_map;
+
+SignalManager::SignalManager(SignalManager::signal_map&& sig_map)
+    : signal_map_(std::move(sig_map)) {
     sigemptyset(&sigset_);
-    sigaddset(&sigset_, SIGINT);
-    sigaddset(&sigset_, SIGTERM);
+
+    for_each_entry([this](const auto& handler_entry) {
+        if (handler_entry) {
+            sigaddset(&sigset_, handler_entry.value);
+        }
+    });
+
     pthread_sigmask(SIG_BLOCK, &sigset_, NULL);
 }
 
 int SignalManager::wait() {
-    int sig = 0;
-    return sigwait(&sigset_, &sig);
+    int result = 0;
+    bool should_continue = false;
+
+    do {
+        int sig = 0;
+        result = sigwait(&sigset_, &sig);
+        for_each_entry([&should_continue, sig](const auto& handler_entry) {
+            if (sig == handler_entry.value && handler_entry) {
+                should_continue = handler_entry(sig);
+            }
+        });
+    } while (should_continue);
+
+    return result;
+}
+
+template <typename Callable>
+void SignalManager::for_each_entry(Callable&& c) {
+    static constexpr auto pmd_tuple = std::make_tuple(&signal_map::sigint_handler,
+                                                      &signal_map::sigterm_handler,
+                                                      &signal_map::sigusr1_handler,
+                                                      &signal_map::sigusr2_handler);
+
+    namespace mp11 = boost::mp11;
+    mp11::tuple_for_each(
+        mp11::tuple_transform([this](auto ptr) -> const auto& { return signal_map_.*ptr; },
+                              pmd_tuple),
+        std::forward<Callable>(c));
 }
 
 }  // namespace sdk
--- src/viam/sdk/module/signal_manager.hpp
+++ src/viam/sdk/module/signal_manager.hpp
@@ -2,24 +2,72 @@
 
 #include <signal.h>
 
+#include <functional>
+
+#include <boost/hana/map.hpp>
+
 namespace viam {
 namespace sdk {
 
 /// @class SignalManager
 /// @brief Defines handling logic for SIGINT and SIGTERM required by all C++
-/// modules.
+/// modules, and optionally for handling SIGUSR1 and SIGUSR2.
 /// @ingroup Module
 class SignalManager {
    public:
-    /// @brief Creates a new SignalManager.
-    explicit SignalManager();
+    /// @brief A callable object for handling async signals.
+    /// Will be called with signal number of the signal which is raised. Return true to indicate
+    /// module execution should continue, false if it should stop.
+    using SignalHandler = std::function<bool(int)>;
 
-    /// @brief Wait for SignalManager to receive SIGINT or SIGTERM.
-    /// @return The signal number if successful.
-    /// @throws `std::runtime_error` if the underlying sigwait call was unsuccessful.
+    /// @brief A registry of SignalHandlers for a predefined set of signals.
+    /// A default-constructed instance will block on SIGINT and SIGTERM, exiting when they are
+    /// raised.
+    struct signal_map {
+        template <int signal_value>
+        struct handler_entry : std::integral_constant<int, signal_value> {
+            handler_entry() = default;
+
+            template <typename Callable>
+            handler_entry(Callable&& c) : handler(std::move(c)) {}
+
+            operator bool() const {
+                return static_cast<bool>(handler);
+            }
+
+            bool operator()(int sig) const {
+                return handler(sig);
+            }
+
+            SignalHandler handler;
+        };
+
+        handler_entry<SIGINT> sigint_handler = [](int) { return false; };
+        handler_entry<SIGTERM> sigterm_handler = [](int) { return false; };
+        handler_entry<SIGUSR1> sigusr1_handler{nullptr};
+        handler_entry<SIGUSR2> sigusr2_handler{nullptr};
+    };
+
+    /// @brief Creates a new SignalManager which waits until SIGINT or SIGTERM are received.
+    explicit SignalManager() : SignalManager({}) {}
+
+    /// @brief Creates a SignalManager with blocking set and callback behavior defined by sig_map.
+    SignalManager(signal_map&& sig_map);
+
+    /// @brief Waits on and handles the signals specified by signal_map_.
+    /// @return The return value of the underlying call to POSIX sigwait.
     int wait();
 
+    const signal_map& map() const {
+        return signal_map_;
+    }
+
    private:
+    template <typename Callable>
+    void for_each_entry(Callable&& c);
+
+    signal_map signal_map_;
+
     sigset_t sigset_;
 };
 
