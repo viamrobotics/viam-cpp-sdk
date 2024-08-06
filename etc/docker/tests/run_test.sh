b209ac78d0533f62da9060c4159342f8f374c081
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2024-08-06 15:44:44 -0400

    test building example make and cmake project

--- etc/docker/tests/run_test.sh
+++ etc/docker/tests/run_test.sh
@@ -9,9 +9,25 @@ cmake .. -G Ninja -DVIAMCPPSDK_USE_DYNAMIC_PROTOS=ON \
 		-DVIAMCPPSDK_CLANG_TIDY=ON
 ninja all
 ninja install
-pushd src/viam/sdk/tests
-UBSAN_OPTIONS="print_stacktrace=1" ctest --output-on-failure
+INSTALL_DIR="$(pwd)/install"
+
+# pushd src/viam/sdk/tests
+# UBSAN_OPTIONS="print_stacktrace=1" ctest --output-on-failure
+# popd
+# pushd src/viam/examples/modules/complex
+# UBSAN_OPTIONS="print_stacktrace=1" ctest --output-on-failure
+# popd
+
+# In the examples below, we check that example_module exits with a status code of 1, 
+# the expected error for not providing a socket address on the command line.
+
+cd ../src/viam/examples/project
+pushd cmake
+cmake . -G Ninja # Just do an in-source build to save path fiddling
+ninja all
+(set +e; ./example_module; [ $? -eq 1 ])
 popd
-pushd src/viam/examples/modules/complex
-UBSAN_OPTIONS="print_stacktrace=1" ctest --output-on-failure
+pushd pkg-config
+PKG_CONFIG_PATH=${INSTALL_DIR}/lib/pkgconfig make all
+(set +e; ./example_module; [ $? -eq 1 ])
 popd
