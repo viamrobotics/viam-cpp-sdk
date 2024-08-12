fef2b46b3b9de3b2b8d403fe99a8969afbf02c32
lia <167905060+lia-viam@users.noreply.github.com>
2024-08-12 13:28:22 -0400

    RSDK-7440: Build example projects in CI (#276)

--- etc/docker/tests/run_test.sh
+++ etc/docker/tests/run_test.sh
@@ -9,9 +9,26 @@ cmake .. -G Ninja -DVIAMCPPSDK_USE_DYNAMIC_PROTOS=ON \
 		-DVIAMCPPSDK_CLANG_TIDY=ON
 ninja all
 ninja install
+INSTALL_DIR="$(pwd)/install"
+
 pushd src/viam/sdk/tests
 UBSAN_OPTIONS="print_stacktrace=1" ctest --output-on-failure
 popd
 pushd src/viam/examples/modules/complex
 UBSAN_OPTIONS="print_stacktrace=1" ctest --output-on-failure
+popd
+
+# Test that example_module builds and runs with the SDK install from above.
+# Check with both CMake and make/pkg-config that we can build the example
+# and have it exit with the expected error message.
+
+cd ../src/viam/examples/project
+pushd cmake
+cmake . -G Ninja # Just do an in-source build to save path fiddling
+ninja all
+[ $(./example_module 2>&1 | grep 'main failed with exception:' -c) = 1 ]
+popd
+pushd pkg-config
+PKG_CONFIG_PATH=${INSTALL_DIR}/lib/pkgconfig make all
+[ $(./example_module 2>&1 | grep 'main failed with exception:' -c) = 1 ]
 popd
