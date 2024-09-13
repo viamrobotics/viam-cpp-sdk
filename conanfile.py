afbe493b299b081afa0aee177ca4f97d3c06c75b
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2024-09-13 15:56:49 -0400

    fix shared and add transitive_headers

--- conanfile.py
+++ conanfile.py
@@ -8,36 +8,41 @@ class ViamCppSdkRecipe(ConanFile):
     license = "Apache-2.0"
     url = "https://github.com/viamrobotics/viam-cpp-sdk/"
 
-    # package_type = "library"
+    package_type = "library"
 
     settings = "os", "compiler", "build_type", "arch"
 
     options = {
         "offline_proto_generation": [True, False],
-        "use_dynamic_protos": [True, False]
-        # , "shared": [True, False]
+        "use_dynamic_protos": [True, False],
+        "shared": [True, False],
+        "propagate_shared": [True, False]
     }
 
     default_options = {
         "offline_proto_generation": False,
-        "use_dynamic_protos": True
-        # , "shared": True
+        "use_dynamic_protos": True,
+        "shared": True,
+        "propagate_shared": True
     }
 
     exports_sources = "CMakeLists.txt", "LICENSE", "src/*"
 
-    # def configure(self):
-        # self.options["*"].shared = self.options.shared
+    def configure(self):
+        # By default we want to propagate the `shared`ness of this package:
+        # if we are shared, build the world as shared, and conversely.
+        if self.options.propagate_shared:
+            self.options["*"].shared = self.options.shared
 
     def requirements(self):
-        self.requires('boost/[>=1.74.0]')
+        self.requires('boost/[>=1.74.0]', transitive_headers=True)
 
         # The SDK supports older grpc and protobuf, but these are the oldest
         # maintained conan packages.
-        self.requires('grpc/[>=1.48.4]')
-        self.requires('protobuf/[>=3.17.1]')
+        self.requires('grpc/[>=1.48.4]', transitive_headers=True)
+        self.requires('protobuf/[>=3.17.1]', transitive_headers=True)
 
-        self.requires('xtensor/[>=0.24.3]')
+        self.requires('xtensor/[>=0.24.3]', transitive_headers=True)
         self.requires('abseil/[>=20230125.3]')
 
     def build_requirements(self):
