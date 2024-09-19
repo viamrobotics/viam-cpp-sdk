ec803d907384fb5d5636e83fd668b8ef7a6c45ff
Abe Winter <abe-winter@users.noreply.github.com>
2024-09-19 15:31:06 -0400

    lint?

--- src/viam/examples/dial_api_key/example_dial_api_key.cpp
+++ src/viam/examples/dial_api_key/example_dial_api_key.cpp
@@ -29,11 +29,11 @@ namespace po = boost::program_options;
 
 int main(int argc, char* argv[]) {
     po::options_description desc("Allowed options");
-    desc.add_options()
-        ("help", "List options and exit")
-        ("uri", po::value<std::string>(), "URI of robot")
-        ("entity", po::value<std::string>(), "api key ID")
-        ("api-key", po::value<std::string>(), "api key secret");
+    desc.add_options()(
+        "help", "List options and exit")(
+        "uri", po::value<std::string>(), "URI of robot")(
+        "entity", po::value<std::string>(), "api key ID")(
+        "api-key", po::value<std::string>(), "api key secret");
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     if (vm.count("help")) {
