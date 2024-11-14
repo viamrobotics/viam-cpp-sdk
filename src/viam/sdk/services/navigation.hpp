4783dd6969a64f8ee4514b8f325009e23a005e76
Abe Winter <abe-winter@users.noreply.github.com>
2024-11-13 19:24:21 -0500

    proto constructors, unique_ptr

--- src/viam/sdk/services/navigation.hpp
+++ src/viam/sdk/services/navigation.hpp
@@ -37,12 +37,19 @@ class Navigation : public Service {
     };
 
     struct Waypoint {
-       public:
+       Waypoint(const viam::service::navigation::v1::Waypoint& proto) {
+        this->id = proto.id();
+        this->location = proto.location();
+       }
         std::string id;
         GeoPoint location;
     };
 
     struct Path {
+        Path(const viam::service::navigation::v1::Path& proto) {
+            this->destination_waypoint_id = proto.destination_waypoint_id();
+            this->geopoints = std::vector<GeoPoint>(proto.geopoints().begin(), proto.geopoints().end());
+        }
         std::string destination_waypoint_id;
         std::vector<GeoPoint> geopoints;
     };
@@ -52,11 +59,11 @@ class Navigation : public Service {
     virtual Mode get_mode(const std::string name, const ProtoStruct& extra) = 0;
     virtual void set_mode(const std::string name, const Mode mode, const ProtoStruct& extra) = 0;
     virtual LocationResponse get_location(const std::string name, const ProtoStruct& extra) = 0;
-    virtual std::vector<Waypoint> get_waypoints(const std::string name, const ProtoStruct& extra) = 0;
+    virtual std::unique_ptr<std::vector<Waypoint>> get_waypoints(const std::string name, const ProtoStruct& extra) = 0;
     virtual void add_waypoint(const std::string name, const GeoPoint& location, const ProtoStruct& extra) = 0;
     virtual void remove_waypoint(const std::string name, const std::string id, const ProtoStruct& extra) = 0;
-    virtual std::vector<GeoGeometry> get_obstacles(const std::string name, const ProtoStruct& extra) = 0;
-    virtual std::vector<Path> get_paths(const std::string name, const ProtoStruct& extra) = 0;
+    virtual std::unique_ptr<std::vector<GeoGeometry>> get_obstacles(const std::string name, const ProtoStruct& extra) = 0;
+    virtual std::unique_ptr<std::vector<Path>> get_paths(const std::string name, const ProtoStruct& extra) = 0;
     virtual MapType get_properties(const std::string) = 0;
     virtual ProtoStruct do_command(const ProtoStruct& command) = 0;
 
