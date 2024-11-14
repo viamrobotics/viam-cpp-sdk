0cb6b02e51e61331aeeb6ddaa66a790160671b32
Abe Winter <abe-winter@users.noreply.github.com>
2024-11-13 19:24:38 -0500

    nav client compiles

---
+++ src/viam/sdk/services/private/navigation_client.cpp
@@ -0,0 +1,152 @@
+#include <viam/sdk/services/private/navigation_client.hpp>
+
+#include <math.h>
+
+#include <grpcpp/support/status.h>
+
+#include <viam/api/service/navigation/v1/navigation.grpc.pb.h>
+#include <viam/api/service/navigation/v1/navigation.pb.h>
+
+#include <viam/sdk/common/client_helper.hpp>
+#include <viam/sdk/common/proto_value.hpp>
+#include <viam/sdk/common/utils.hpp>
+#include <viam/sdk/services/navigation.hpp>
+
+namespace viam {
+namespace sdk {
+namespace impl {
+
+using namespace viam::service::navigation::v1;
+
+NavigationClient::NavigationClient(std::string name, std::shared_ptr<grpc::Channel> channel)
+    : Navigation(std::move(name)),
+      stub_(service::navigation::v1::NavigationService::NewStub(channel)),
+      channel_(std::move(channel)) {};
+
+Navigation::Mode NavigationClient::get_mode(const std::string name, const ProtoStruct& extra) {
+    return make_client_helper(this, *stub_, &StubType::GetMode)
+        .with([&](auto& request) {
+            *request.mutable_name() = name;
+            *request.mutable_extra() = map_to_struct(extra);
+        })
+        .invoke([](auto& response) { return Navigation::Mode(response.mode()); });
+}
+
+void NavigationClient::set_mode(const std::string name,
+                                const Navigation::Mode mode,
+                                const ProtoStruct& extra) {
+    return make_client_helper(this, *stub_, &StubType::SetMode)
+        .with([&](auto& request) {
+            *request.mutable_name() = name;
+            request.set_mode(viam::service::navigation::v1::Mode(mode));
+            *request.mutable_extra() = map_to_struct(extra);
+        })
+        .invoke([](auto& response) {});
+}
+
+Navigation::LocationResponse NavigationClient::get_location(const std::string name,
+                                                            const ProtoStruct& extra) {
+    return make_client_helper(this, *stub_, &StubType::GetLocation)
+        .with([&](auto& request) {
+            *request.mutable_name() = name;
+            *request.mutable_extra() = map_to_struct(extra);
+        })
+        .invoke([](auto& response) {
+            return Navigation::LocationResponse{
+                response.location(),
+                response.compass_heading(),
+            };
+        });
+}
+
+// helper; unpacks a repeated pointer to a vector of destination type.
+// Dst must have a constructor that takes Src*.
+template <typename Src, typename Dst>
+std::unique_ptr<std::vector<Dst>> repeatedPtrToVector(
+    const ::google::protobuf::RepeatedPtrField<Src>& items) {
+    auto vec = std::make_unique<std::vector<Dst>>();
+    vec->reserve(items.size());
+    for (auto& x : items) {
+        vec->push_back(x);
+    }
+    return vec;
+}
+
+std::unique_ptr<std::vector<Navigation::Waypoint>> NavigationClient::get_waypoints(
+    const std::string name, const ProtoStruct& extra) {
+    return make_client_helper(this, *stub_, &StubType::GetWaypoints)
+        .with([&](auto& request) {
+            *request.mutable_name() = name;
+            *request.mutable_extra() = map_to_struct(extra);
+        })
+        .invoke([](auto& response) {
+            return repeatedPtrToVector<viam::service::navigation::v1::Waypoint,
+                                       Navigation::Waypoint>(response.waypoints());
+        });
+}
+
+void NavigationClient::add_waypoint(const std::string name,
+                                    const GeoPoint& location,
+                                    const ProtoStruct& extra) {
+    return make_client_helper(this, *stub_, &StubType::AddWaypoint)
+        .with([&](auto& request) {
+            *request.mutable_name() = name;
+            *request.mutable_location() = location;
+            *request.mutable_extra() = map_to_struct(extra);
+        })
+        .invoke([](auto& response) {});
+}
+
+void NavigationClient::remove_waypoint(const std::string name,
+                                       const std::string id,
+                                       const ProtoStruct& extra) {
+    return make_client_helper(this, *stub_, &StubType::RemoveWaypoint)
+        .with([&](auto& request) {
+            *request.mutable_name() = name;
+            *request.mutable_id() = id;
+            *request.mutable_extra() = map_to_struct(extra);
+        })
+        .invoke([](auto& response) {});
+}
+
+std::unique_ptr<std::vector<GeoGeometry>> NavigationClient::get_obstacles(
+    const std::string name, const ProtoStruct& extra) {
+    return make_client_helper(this, *stub_, &StubType::GetObstacles)
+        .with([&](auto& request) {
+            *request.mutable_name() = name;
+            *request.mutable_extra() = map_to_struct(extra);
+        })
+        .invoke([](auto& response) {
+            return std::make_unique<std::vector<GeoGeometry>>(response.obstacles().begin(),
+                                                              response.obstacles().end());
+        });
+}
+
+std::unique_ptr<std::vector<NavigationClient::Path>> NavigationClient::get_paths(
+    const std::string name, const ProtoStruct& extra) {
+    return make_client_helper(this, *stub_, &StubType::GetPaths)
+        .with([&](auto& request) {
+            *request.mutable_name() = name;
+            *request.mutable_extra() = map_to_struct(extra);
+        })
+        .invoke([](auto& response) {
+            return std::make_unique<std::vector<Path>>(response.paths().begin(),
+                                                       response.paths().end());
+        });
+}
+
+NavigationClient::MapType NavigationClient::get_properties(const std::string name) {
+    return make_client_helper(this, *stub_, &StubType::GetProperties)
+        .with([&](auto& request) { *request.mutable_name() = name; })
+        .invoke([](auto& response) { return MapType(response.map_type()); });
+}
+
+ProtoStruct NavigationClient::do_command(const ProtoStruct& command) {
+    return make_client_helper(this, *stub_, &StubType::DoCommand)
+        .with([&](auto& request) { *request.mutable_command() = map_to_struct(command); })
+        .invoke([](auto& response) { return struct_to_map(response.result()); });
+}
+
+}  // namespace impl
+}  // namespace sdk
+}  // namespace viam
---
+++ src/viam/sdk/services/private/navigation_client.hpp
@@ -0,0 +1,48 @@
+/// @file services/navigation/client.hpp
+///
+/// @brief Implements a gRPC client for the `Navigation` service.
+#pragma once
+
+#include <grpcpp/channel.h>
+
+#include <viam/api/service/navigation/v1/navigation.grpc.pb.h>
+
+#include <viam/sdk/services/navigation.hpp>
+
+namespace viam {
+namespace sdk {
+namespace impl {
+
+/// @class NavigationClient
+/// @brief gRPC client implementation of a `Navigation` service.
+/// @ingroup Navigation
+class NavigationClient : public Navigation {
+   public:
+    using interface_type = Navigation;
+    NavigationClient(std::string name, std::shared_ptr<grpc::Channel> channel);
+
+    Mode get_mode(const std::string name, const ProtoStruct& extra) override;
+    void set_mode(const std::string name, const Mode mode, const ProtoStruct& extra) override;
+    LocationResponse get_location(const std::string name, const ProtoStruct& extra) override;
+    std::unique_ptr<std::vector<Waypoint>> get_waypoints(const std::string name, const ProtoStruct& extra) override;
+    void add_waypoint(const std::string name,
+                      const GeoPoint& location,
+                      const ProtoStruct& extra) override;
+    void remove_waypoint(const std::string name,
+                         const std::string id,
+                         const ProtoStruct& extra) override;
+    std::unique_ptr<std::vector<GeoGeometry>> get_obstacles(const std::string name,
+                                           const ProtoStruct& extra) override;
+    std::unique_ptr<std::vector<Path>> get_paths(const std::string name, const ProtoStruct& extra) override;
+    MapType get_properties(const std::string name) override;
+    ProtoStruct do_command(const ProtoStruct& command) override;
+
+   private:
+    using StubType = service::navigation::v1::NavigationService::StubInterface;
+    std::unique_ptr<StubType> stub_;
+    std::shared_ptr<grpc::Channel> channel_;
+};
+
+}  // namespace impl
+}  // namespace sdk
+}  // namespace viam
