7b7da5a99b00ee3cf135f0aef6f36a5f8d471565
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2024-12-03 10:26:58 -0500

    remove ctor semicolon

--- src/viam/sdk/components/private/power_sensor_client.cpp
+++ src/viam/sdk/components/private/power_sensor_client.cpp
@@ -38,7 +38,7 @@ PowerSensor::current from_proto(const GetCurrentResponse& proto) {
 PowerSensorClient::PowerSensorClient(std::string name, std::shared_ptr<grpc::Channel> channel)
     : PowerSensor(std::move(name)),
       stub_(PowerSensorService::NewStub(channel)),
-      channel_(std::move(channel)) {};
+      channel_(std::move(channel)) {}
 
 PowerSensor::voltage PowerSensorClient::get_voltage(const ProtoStruct& extra) {
     return make_client_helper(this, *stub_, &StubType::GetVoltage)
