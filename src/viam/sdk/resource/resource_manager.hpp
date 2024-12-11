0729e8793935ffd76bce0f3fa6cef4c30c1495d3
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2024-12-11 13:07:24 -0500

    clean up resource_manager includes

--- src/viam/sdk/resource/resource_manager.hpp
+++ src/viam/sdk/resource/resource_manager.hpp
@@ -9,9 +9,6 @@
 #include <unordered_map>
 
 #include <boost/optional/optional.hpp>
-#include <grpcpp/impl/service_type.h>
-
-#include <viam/api/component/generic/v1/generic.grpc.pb.h>
 
 #include <viam/sdk/resource/resource.hpp>
 #include <viam/sdk/resource/resource_api.hpp>
@@ -63,7 +60,7 @@ class ResourceManager {
     /// @brief Returns a reference to the existing resources within the manager.
     const std::unordered_map<std::string, std::shared_ptr<Resource>>& resources() const;
 
-    ResourceManager(){};
+    ResourceManager() {};
 
    private:
     std::mutex lock_;
