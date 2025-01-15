42d39169484e083f4a00f2f583b449e7f4453b3f
lia-viam <lia-viam@users.noreply.github.com>
2025-01-15 15:39:34 +0000

    deploy: 7ab3a9705e7d0e50f1dcdad2d362d9cf52b58b9a

--- search/classes_3.js
+++ search/classes_3.js
@@ -2,7 +2,5 @@ var searchData=
 [
   ['depth_5fmap_0',['depth_map',['../structdepth__map.html',1,'']]],
   ['dialoptions_1',['DialOptions',['../classviam_1_1sdk_1_1DialOptions.html',1,'viam::sdk']]],
-  ['discovery_2',['discovery',['../structviam_1_1sdk_1_1RobotClient_1_1discovery.html',1,'viam::sdk::RobotClient']]],
-  ['discovery_5fquery_3',['discovery_query',['../structviam_1_1sdk_1_1RobotClient_1_1discovery__query.html',1,'viam::sdk::RobotClient']]],
-  ['distortion_5fparameters_4',['distortion_parameters',['../structviam_1_1sdk_1_1Camera_1_1distortion__parameters.html',1,'viam::sdk::Camera']]]
+  ['distortion_5fparameters_2',['distortion_parameters',['../structviam_1_1sdk_1_1Camera_1_1distortion__parameters.html',1,'viam::sdk::Camera']]]
 ];
