f2cae062213cc997a4e7bbb56bef0d2aa0ae1e42
Bijan Haney <bijan@viam.com>
2025-03-27 13:48:49 -0400

    small tweak

--- src/viam/sdk/services/private/mlmodel_server.cpp
+++ src/viam/sdk/services/private/mlmodel_server.cpp
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 #include <viam/sdk/services/private/mlmodel_server.hpp>
 
 #include <viam/sdk/common/private/service_helper.hpp>
