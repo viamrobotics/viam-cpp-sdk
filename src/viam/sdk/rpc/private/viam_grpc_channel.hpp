c549c07188396d2b9bf9aafeaaeffda88c8c13f6
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2025-04-22 09:51:37 -0400

    docs

--- src/viam/sdk/rpc/private/viam_grpc_channel.hpp
+++ src/viam/sdk/rpc/private/viam_grpc_channel.hpp
@@ -13,6 +13,9 @@ namespace impl {
 /// size kMaxMessageSize.
 std::shared_ptr<grpc::Channel> create_viam_channel(
     const grpc::string& target, const std::shared_ptr<grpc::ChannelCredentials>& credentials);
+
+/// @brief Like grpc::CreateChannel, but for the express purpose of returning a channel for making
+/// an AuthService request.
 std::shared_ptr<grpc::Channel> create_viam_auth_channel(const std::string& address);
 
 }  // namespace impl
