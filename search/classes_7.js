70ba9d847fa5057d920cab2a7400ef574383a02e
stuqdog <stuqdog@users.noreply.github.com>
2025-06-22 13:12:49 +0000

    deploy: d7ac685d2dfc3cdb0ac9677bf43c2800a6fb8d2f

--- search/classes_7.js
+++ search/classes_7.js
@@ -3,5 +3,6 @@ var searchData=
   ['handlermap_5f_0',['HandlerMap_',['../classviam_1_1sdk_1_1HandlerMap__.html',1,'viam::sdk']]],
   ['hash_3c_3a_3aviam_3a_3asdk_3a_3aapi_20_3e_1',['hash&lt;::viam::sdk::API &gt;',['../structstd_1_1hash_3_1_1viam_1_1sdk_1_1API_01_4.html',1,'std']]],
   ['hash_3c_3a_3aviam_3a_3asdk_3a_3aname_20_3e_2',['hash&lt;::viam::sdk::Name &gt;',['../structstd_1_1hash_3_1_1viam_1_1sdk_1_1Name_01_4.html',1,'std']]],
-  ['hash_3c_3a_3aviam_3a_3asdk_3a_3arpcsubtype_20_3e_3',['hash&lt;::viam::sdk::RPCSubtype &gt;',['../structstd_1_1hash_3_1_1viam_1_1sdk_1_1RPCSubtype_01_4.html',1,'std']]]
+  ['hash_3c_3a_3aviam_3a_3asdk_3a_3arpcsubtype_20_3e_3',['hash&lt;::viam::sdk::RPCSubtype &gt;',['../structstd_1_1hash_3_1_1viam_1_1sdk_1_1RPCSubtype_01_4.html',1,'std']]],
+  ['holding_5fstatus_4',['holding_status',['../structviam_1_1sdk_1_1Gripper_1_1holding__status.html',1,'viam::sdk::Gripper']]]
 ];
