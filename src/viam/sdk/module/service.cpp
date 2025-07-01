b5578cefbc168336a43d186aa14f57f3d0c1ca70
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2025-07-01 15:26:58 -0400

    error on model reg not found

--- src/viam/sdk/module/service.cpp
+++ src/viam/sdk/module/service.cpp
@@ -83,7 +83,8 @@ struct ModuleService::ServiceImpl : viam::module::v1::ModuleService::Service {
             } catch (const std::exception& exc) {
                 return grpc::Status(::grpc::INTERNAL, exc.what());
             }
-        };
+        }
+
         try {
             parent.server_->add_resource(res, ctx->deadline());
         } catch (const std::exception& exc) {
@@ -131,13 +132,16 @@ struct ModuleService::ServiceImpl : viam::module::v1::ModuleService::Service {
         const std::shared_ptr<const ModelRegistration> reg =
             Registry::get().lookup_model(cfg.api(), cfg.model());
 
-        if (reg) {
-            try {
-                std::shared_ptr<Resource> resource = reg->construct_resource(deps, cfg);
-                manager->replace_one(cfg.resource_name(), std::move(resource));
-            } catch (const std::exception& exc) {
-                return grpc::Status(::grpc::INTERNAL, exc.what());
-            }
+        if (!reg) {
+            return grpc::Status(::grpc::INTERNAL,
+                                "Unable to rebuild resource: model registration not found");
+        }
+
+        try {
+            std::shared_ptr<Resource> resource = reg->construct_resource(deps, cfg);
+            manager->replace_one(cfg.resource_name(), std::move(resource));
+        } catch (const std::exception& exc) {
+            return grpc::Status(::grpc::INTERNAL, exc.what());
         }
 
         return grpc::Status();
