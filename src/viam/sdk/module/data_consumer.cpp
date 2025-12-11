5b3615079e6ba5b3f3f495011a8a6581ee7f6fcb
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2025-12-11 17:35:52 -0500

    skeleton of historical data less bson

--- src/viam/sdk/module/data_consumer.cpp
+++ src/viam/sdk/module/data_consumer.cpp
@@ -1,1 +1,23 @@
 #include <viam/sdk/module/data_consumer.hpp>
+
+#include <viam/sdk/common/utils.hpp>
+
+namespace viam {
+namespace sdk {
+
+DataConsumer::DataConsumer(DataClient& dc) : client_(dc) {}
+
+std::vector<DataClient::MQLBinary> DataConsumer::query_tabular_data(
+    const std::string& resource, DataConsumer::QueryOptions opts) {
+    const std::string org_id = get_env("VIAM_PRIMARY_ORG_ID");
+    const std::string part_id = get_env("VIAM_MACHINE_PART_ID");
+
+    DataClient::MQLBinary base_query;
+
+    opts.additional_stages.insert(opts.additional_stages.begin(), std::move(base_query));
+
+    return client_.tabular_data_by_mql(org_id, opts.additional_stages, {});
+}
+
+}  // namespace sdk
+}  // namespace viam
--- src/viam/sdk/module/data_consumer.hpp
+++ src/viam/sdk/module/data_consumer.hpp
@@ -1,11 +1,32 @@
 #pragma once
 
+#include <cstdint>
+
+#include <chrono>
+#include <vector>
+
+#include <viam/sdk/app/data_client.hpp>
+
 namespace viam {
 namespace sdk {
 
 class DataConsumer {
    public:
+    struct QueryOptions {
+        /// @brief How far back should the data be included.
+        std::chrono::milliseconds time_back;
+
+        /// @brief Any additional query stages to be included after the default query.
+        std::vector<DataClient::MQLBinary> additional_stages;
+    };
+
+    DataConsumer(DataClient& dc);
+
+    std::vector<DataClient::MQLBinary> query_tabular_data(const std::string& resource,
+                                                          QueryOptions opts);
+
    private:
+    DataClient& client_;
 };
 
 }  // namespace sdk
