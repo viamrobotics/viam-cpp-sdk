a787dd4ce2e5c9ac159836b6fb6ff47ae78c24e3
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2025-12-18 18:21:31 -0500

    add test case

--- src/viam/sdk/tests/test_data_consumer.cpp
+++ src/viam/sdk/tests/test_data_consumer.cpp
@@ -10,17 +10,29 @@ namespace sdktests {
 namespace data_consumer {
 
 BOOST_AUTO_TEST_CASE(test_default_query) {
-    auto now =
-        std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::system_clock::now());
-    auto result = sdk::DataConsumer::default_query("part", "res", now);
+    using time_point_type = std::chrono::system_clock::time_point;
 
-    std::cout << "result size " << result.size() << "\n";
+    auto new_years_2025 = time_point_type{} + std::chrono::milliseconds{1735689600000};
+
+    auto result = sdk::DataConsumer::default_query(
+        "part", "res", std::chrono::time_point_cast<std::chrono::milliseconds>(new_years_2025));
+
+    // This is an arbitary input/output that I computed and validated with
+    // https://jsontotable.org/bson-to-json to make sure that I can convert it to JSON
+    const std::string expected =
+        "5e00000003246d61746368005100000002706172745f69640005000000706172740002636f6d706f6e656e745f"
+        "6e616d650004000000726573000374696d655f72656365697665640013000000092467746500007c291f940100"
+        "00000000";
+
+    std::ostringstream os;
 
     for (auto b : result) {
-        std::cout << std::hex << std::setfill('0') << std::setw(2) << unsigned(b);
+        os << std::hex << std::setfill('0') << std::setw(2) << unsigned(b);
     }
 
-    std::cout << "\n";
+    const std::string actual = os.str();
+
+    BOOST_CHECK_EQUAL(expected, actual);
 }
 
 }  // namespace data_consumer
