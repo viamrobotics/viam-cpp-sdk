9b3fbae692904b0147f9842a7068c6e3fd9fede6
lia-viam <lia-viam@users.noreply.github.com>
2026-01-09 19:11:19 +0000

    deploy: 64c35949ae8e08611aa034a20154da237e493147

--- search/all_14.js
+++ search/all_14.js
@@ -6,12 +6,13 @@ var searchData=
   ['vector3_3',['Vector3',['../structviam_1_1sdk_1_1Vector3.html',1,'viam::sdk']]],
   ['viam_20c_20sdk_4',['Viam C++ SDK',['../index.html',1,'']]],
   ['viam_3a_3asdk_3a_3aaudio_5fcodecs_5',['audio_codecs',['../namespaceviam_1_1sdk_1_1audio__codecs.html',1,'viam::sdk']]],
-  ['viam_5fresource_5flog_6',['VIAM_RESOURCE_LOG',['../group__Log.html#ga5f00547ef3446074c72172c83983e26b',1,'logging.hpp']]],
-  ['viam_5fsdk_5flog_7',['VIAM_SDK_LOG',['../group__Log.html#gab35bc9dc93a02ef947130b9d23e8a051',1,'logging.hpp']]],
-  ['viamchannel_8',['ViamChannel',['../classviam_1_1sdk_1_1ViamChannel.html',1,'viam::sdk']]],
-  ['viamclient_9',['ViamClient',['../classviam_1_1sdk_1_1ViamClient.html',1,'viam::sdk']]],
-  ['vision_5fservice_10',['vision_service',['../structviam_1_1sdk_1_1obstacle__detector.html#a2244bb582533a6a0489d2066c4048a68',1,'viam::sdk::obstacle_detector']]],
-  ['visit_20api_11',['ProtoValue visit API',['../group__ProtoValueVisit.html',1,'']]],
-  ['voltage_12',['voltage',['../structviam_1_1sdk_1_1PowerSensor_1_1voltage.html',1,'viam::sdk::PowerSensor']]],
-  ['volts_13',['volts',['../structviam_1_1sdk_1_1PowerSensor_1_1voltage.html#a14f378a6d53d4514a340efe1526da02e',1,'viam::sdk::PowerSensor::voltage']]]
+  ['viam_5fmodule_5flog_6',['VIAM_MODULE_LOG',['../group__Log.html#ga0e0bd45893e33ca916171e23d45bfce1',1,'logging.hpp']]],
+  ['viam_5fresource_5flog_7',['VIAM_RESOURCE_LOG',['../group__Log.html#ga5f00547ef3446074c72172c83983e26b',1,'logging.hpp']]],
+  ['viam_5fsdk_5flog_8',['VIAM_SDK_LOG',['../group__Log.html#gab35bc9dc93a02ef947130b9d23e8a051',1,'logging.hpp']]],
+  ['viamchannel_9',['ViamChannel',['../classviam_1_1sdk_1_1ViamChannel.html',1,'viam::sdk']]],
+  ['viamclient_10',['ViamClient',['../classviam_1_1sdk_1_1ViamClient.html',1,'viam::sdk']]],
+  ['vision_5fservice_11',['vision_service',['../structviam_1_1sdk_1_1obstacle__detector.html#a2244bb582533a6a0489d2066c4048a68',1,'viam::sdk::obstacle_detector']]],
+  ['visit_20api_12',['ProtoValue visit API',['../group__ProtoValueVisit.html',1,'']]],
+  ['voltage_13',['voltage',['../structviam_1_1sdk_1_1PowerSensor_1_1voltage.html',1,'viam::sdk::PowerSensor']]],
+  ['volts_14',['volts',['../structviam_1_1sdk_1_1PowerSensor_1_1voltage.html#a14f378a6d53d4514a340efe1526da02e',1,'viam::sdk::PowerSensor::voltage']]]
 ];
