3fc02f40a114e1fcc8a3345006153663df24c3ec
npmenard <npmenard@users.noreply.github.com>
2026-03-02 21:58:16 +0000

    deploy: 1ba61175ea465566d3039fa44caee595a5327aec

--- search/typedefs_3.js
+++ search/typedefs_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['mime_5ftypes_0',['mime_types',['../classviam_1_1sdk_1_1Camera.html#ad07b74c95a1fb9435323b4bf590112cd',1,'viam::sdk::Camera']]]
+  ['mime_5ftypes_0',['mime_types',['../classviam_1_1sdk_1_1Camera.html#ad07b74c95a1fb9435323b4bf590112cd',1,'viam::sdk::Camera']]],
+  ['movelimit_1',['MoveLimit',['../classviam_1_1sdk_1_1Arm.html#a43d85acd5248bca5d8911a54e539958b',1,'viam::sdk::Arm']]]
 ];
