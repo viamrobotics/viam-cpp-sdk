6899e58fcc092ea6fd4d9114c8de7bec8e91aba4
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2026-03-23 11:58:35 -0400

    deprecate reconfigurable but restore its method

--- src/viam/sdk/resource/reconfigurable.hpp
+++ src/viam/sdk/resource/reconfigurable.hpp
@@ -6,9 +6,17 @@
 namespace viam {
 namespace sdk {
 
-class Reconfigurable {
+class [[deprecated(
+    "Reconfigure is no longer part of the Resource interface. You can still implement it if you "
+    "like, but viam-server will rebuild resources on configuration change rather than calling a "
+    "reconfigure method.")]] Reconfigurable {
    public:
     virtual ~Reconfigurable();
+
+    /// @brief Reconfigures a resource.
+    /// @param deps Dependencies of the resource.
+    /// @param cfg The resource's config.
+    virtual void reconfigure(const Dependencies& deps, const ResourceConfig& cfg) = 0;
 
    protected:
     explicit Reconfigurable();
